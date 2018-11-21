eb76242f1d8bb72a2fed0e442fb767b7116dc0c2
Daniel Rozenberg <rodaniel@ampproject.org>
2018-11-21 17:34:17 -0500

    Initial version of bundle-size bot

---
+++ bundle-size/db.js
@@ -0,0 +1,25 @@
+/**
+ * Copyright 2018, the AMP HTML authors
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+const knex = require('knex');
+
+exports.dbConnect = () => {
+  const client = process.env['SQL_CLIENT'];
+  const connection = Object.assign(...Object.keys(process.env)
+      .filter(name => name.startsWith('CLOUD_SQL_'))
+      .map(name => ({[name.substr(10).toLowerCase()]: process.env[name]})));
+  return knex({client, connection});
+}
