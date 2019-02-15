ampproject/amp-github-apps

commit 89b417279681afc5064e11116c0b7495e9b0c1a7

Update bundle-size calculation description text (#62)

bundle-size/app.js
@@ -238,8 +238,8 @@ module.exports = app => {
       output: {
         title: 'Calculating new bundle size for this PR…',
         summary: 'The bundle size (gzipped compressed size of `v0.js`) ' +
-          'of this pull request is being calculated on the ' +
-          '`integration_tests` shard of the Travis build.',
+          'of this pull request is being calculated on Travis. Look for the ' +
+          'shard that contains "Bundle Size" its title.',
       },
     });
     const check = await context.github.checks.create(params);

commit b85e9ef3b747cf817eb7f42d3aac60f7a264a7c2

Fix typo introduced in #62

bundle-size/app.js
@@ -239,7 +239,7 @@ module.exports = app => {
         title: 'Calculating new bundle size for this PR…',
         summary: 'The bundle size (gzipped compressed size of `v0.js`) ' +
           'of this pull request is being calculated on Travis. Look for the ' +
-          'shard that contains "Bundle Size" its title.',
+          'shard that contains "Bundle Size" in its title.',
       },
     });
     const check = await context.github.checks.create(params);