b189e481806954dbb23ab64be9e5060bb8420677
Alan Orozco <orozcoalan@gmail.com>
2019-02-27 01:07:08 -0800

    Encourage bundle-shrinking humans with emoji

--- bundle-size/app.js
+++ bundle-size/app.js
@@ -21,6 +21,8 @@ const sleep = require('sleep-promise');
 const RETRY_MILLIS = 60000;
 const RETRY_TIMES = 60;
 
+const HUMAN_ENCOURAGEMENT_MAX_DELTA = -0.1;
+
 const db = dbConnect();
 
 /**
@@ -66,6 +68,58 @@ async function getOwners(github) {
  */
 function formatBundleSizeDelta(delta) {
   return 'Δ ' + (delta >= 0 ? '+' : '') + delta.toFixed(2) + 'KB';
+}
+
+/**
+ * Returns an encouraging result title when the bundle size is reduced.
+ * Otherwise the title is neutral, but not discouraging.
+ *
+ * @param {number} delta bundle size delta.
+ * @param {string} deltaFormatted bundle size delta from `formatBundleSizeDelta`
+ * @param {string} encouragementEmoji bring in those 絵文字 !
+ * @return {string} successful title message.
+ */
+function successfulTitleMsg(delta, deltaFormatted, encouragementEmoji = '🎉') {
+  const okDesc = 'no approval necessary';
+  if (delta <= HUMAN_ENCOURAGEMENT_MAX_DELTA) {
+    return `${deltaFormatted} ${assertIsEmoji(encouragementEmoji)} | ${okDesc}`;
+  }
+  return `${deltaFormatted} | ${okDesc}`;
+}
+
+/**
+ * Asserts that a string contains only emoji.
+ * @param {string} supposedlyJustEmoji
+ * @return {string}
+ */
+function assertIsEmoji(supposedlyJustEmoji) {
+  const re = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|[\ud83c[\ude50\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
+  if (supposedlyJustEmoji.replace(re, '').length > 0) {
+    throw new Error('Not 絵文字 ! 🤖🤖🤖');
+  }
+  return supposedlyJustEmoji;
+}
+
+/**
+ * Returns an encouraging result summary when the bundle size is reduced.
+ * Otherwise the summary is neutral, but not discouraging.
+ *
+ * @param {number} delta bundle size delta.
+ * @param {string} deltaFormatted bundle size delta from `formatBundleSizeDelta`
+ * @return {string} successful summary message.
+ */
+function successfulSummaryMsg(delta, deltaFormatted) {
+  const sizeDesc = 'gzipped compressed size of `v0.js`';
+  const deltaDesc = `The bundle size change is ${deltaFormatted}.`;
+
+  if (delta <= HUMAN_ENCOURAGEMENT_MAX_DELTA) {
+    return `This pull request *reduces* the bundle size (${sizeDesc}), so no ` +
+      `special approval is necessary. ${deltaDesc}`;
+  }
+
+  return `This pull request does not increase the bundle size (${sizeDesc})` +
+    'by any significant amount, so no special approval is necessary. ' +
+    deltaDesc;
 }
 
 module.exports = app => {
@@ -139,15 +193,13 @@ module.exports = app => {
           },
         });
       } else {
+        const title =
+            successfulTitleMsg(bundleSizeDelta, bundleSizeDeltaFormatted);
+        const summary =
+            successfulSummaryMsg(bundleSizeDelta, bundleSizeDeltaFormatted);
         Object.assign(updatedCheckOptions, {
           conclusion: 'success',
-          output: {
-            title: `${bundleSizeDeltaFormatted} | no approval necessary`,
-            summary: 'This pull request does not increase the bundle size ' +
-              '(gzipped compressed size of `v0.js`) by any significant ' +
-              'amount, so no special approval is necessary. The bundle size ' +
-              `change is ${bundleSizeDeltaFormatted}`,
-          },
+          output: {title, summary},
         });
       }
       await github.checks.update(updatedCheckOptions);
