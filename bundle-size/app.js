f2e5766d6658aabc74bd16216b2c4c39e9e0f1f7
Alan Orozco <alanorozco@users.noreply.github.com>
2019-02-27 08:43:09 -0800

    not funny, alan

--- bundle-size/app.js
+++ bundle-size/app.js
@@ -76,29 +76,14 @@ function formatBundleSizeDelta(delta) {
  *
  * @param {number} delta bundle size delta.
  * @param {string} deltaFormatted bundle size delta from `formatBundleSizeDelta`
- * @param {string=} encouragementEmoji bring in those 絵文字 ! default is '🎉'.
  * @return {string} successful title message.
  */
-function successfulTitleMsg(delta, deltaFormatted, encouragementEmoji = '🎉') {
-  const okDesc = 'no approval necessary';
+function successfulTitleMsg(delta, deltaFormatted) {
+  const noApprovalNecessaryDesc = 'no approval necessary';
   if (delta <= HUMAN_ENCOURAGEMENT_MAX_DELTA) {
-    return `${deltaFormatted} ${assertIsEmoji(encouragementEmoji)} | ${okDesc}`;
+    return `${deltaFormatted} 🎉 | ${noApprovalNecessaryDesc}`;
   }
-  return `${deltaFormatted} | ${okDesc}`;
-}
-
-/**
- * Asserts that a string contains only emoji.
- * @param {string} supposedlyJustEmoji
- * @return {string}
- */
-function assertIsEmoji(supposedlyJustEmoji) {
-  // (Obviously jk about this assertion, will remove.)
-  const re = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|[\ud83c[\ude50\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
-  if (supposedlyJustEmoji.replace(re, '').length > 0) {
-    throw new Error('Not 絵文字 ! 🤖🤖🤖');
-  }
-  return supposedlyJustEmoji;
+  return `${deltaFormatted} | ${noApprovalNecessaryDesc}`;
 }
 
 /**
