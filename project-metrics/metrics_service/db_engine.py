d4085de7a0e9005eca49ffa91c7a0200ca924d19
Ryan Cebulko <ryan@cebulko.com>
2019-06-04 14:56:36 -0400

    Project Metrics: Database schema, config, initialization (#149)
    * SQL Alchemy database engine
    
    * Base metric model definition and table schema
    
    * Database initialization
    
    * Remove extra score level

---
+++ project-metrics/metrics_service/db_engine.py
@@ -0,0 +1,25 @@
+"""Provides the database engine for the CloudSQL instance."""
+
+import sqlalchemy
+from sqlalchemy.orm import session
+import functools
+import env
+
+
+@functools.lru_cache()
+def get_engine() -> sqlalchemy.engine.Engine:
+  return sqlalchemy.create_engine(
+      sqlalchemy.engine.url.URL(
+          drivername=env.get('SQL_DRIVER'),
+          username=env.get('DB_USER'),
+          password=env.get('DB_PASS'),
+          database=env.get('DB_NAME'),
+          query={
+              'unix_socket': '%s/%s' % (
+                  env.get('CLOUD_SQL_SOCKET'),
+                  env.get('CLOUD_SQL_INSTANCE_NAME')),
+              'charset': 'utf8mb4',
+          }
+      ), echo=True)
+
+get_session = session.sessionmaker(bind=get_engine())
---
+++ project-metrics/metrics_service/models.py
@@ -0,0 +1,44 @@
+"""Defines ORM models for interactions with the DB."""
+
+from typing import Text
+import datetime
+import enum
+import sqlalchemy
+from sqlalchemy.ext import declarative
+
+Base = declarative.declarative_base()
+
+
+class MetricScore(enum.Enum):
+  """The computed score of a metric."""
+  UNKNOWN = 0
+  POOR = 1
+  MODERATE = 2
+  GOOD = 3
+  EXCELLENT = 4
+
+
+class MetricResult(Base):
+  """A score computed for a single metric."""
+
+  __tablename__ = 'metric_results'
+  # Composite index on computed_at then name, since queries will be ordering by
+  # timestamp then filtering or DISTINCT-ing by name to always fetch the latest
+  # row for each metric.
+  __table_args__ = (sqlalchemy.Index('computed_at__name', 'computed_at',
+                                     'name'),)
+
+  id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)  # type: int
+  name = sqlalchemy.Column(sqlalchemy.Unicode(255))  # type: Text
+  # Actual "result value" used to determine the score. This may be a number of
+  # PRs, a percentage value, latency in minutes, etc. The metric itself shall
+  # be responsible for interpreting the value, determining the corresponding
+  # score, and rendering the result.
+  value = sqlalchemy.Column(sqlalchemy.Float)  # type: float
+  computed_at = sqlalchemy.Column(
+      sqlalchemy.DateTime,
+      default=datetime.datetime.utcnow)  # type: datetime.datetime
+
+  def __repr__(self) -> Text:
+    return "<Metric(name='%s', value='%.1f', computed_at='%s')>" % (
+        self.name, self.value, self.computed_at)
