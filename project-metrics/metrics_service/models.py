2a25ff9fd846a60c7d4a0553309dc6b1d262edb3
Ryan Cebulko <ryan@cebulko.com>
2019-06-04 14:12:59 -0400

    Remove extra score level

--- project-metrics/metrics_service/models.py
+++ project-metrics/metrics_service/models.py
@@ -4,7 +4,7 @@ from typing import Text
 import datetime
 import enum
 import sqlalchemy
-from sqlalchemy.ext import declarative 
+from sqlalchemy.ext import declarative
 
 Base = declarative.declarative_base()
 
@@ -12,11 +12,10 @@ Base = declarative.declarative_base()
 class MetricScore(enum.Enum):
   """The computed score of a metric."""
   UNKNOWN = 0
-  CRITICAL = 1
-  POOR = 2
-  MODERATE = 3
-  GOOD = 4
-  EXCELLENT = 5
+  POOR = 1
+  MODERATE = 2
+  GOOD = 3
+  EXCELLENT = 4
 
 
 class MetricResult(Base):
@@ -26,8 +25,8 @@ class MetricResult(Base):
   # Composite index on computed_at then name, since queries will be ordering by
   # timestamp then filtering or DISTINCT-ing by name to always fetch the latest
   # row for each metric.
-  __table_args__ = (sqlalchemy.Index(
-    'computed_at__name', 'computed_at', 'name'),)
+  __table_args__ = (sqlalchemy.Index('computed_at__name', 'computed_at',
+                                     'name'),)
 
   id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)  # type: int
   name = sqlalchemy.Column(sqlalchemy.Unicode(255))  # type: Text
@@ -37,7 +36,8 @@ class MetricResult(Base):
   # score, and rendering the result.
   value = sqlalchemy.Column(sqlalchemy.Float)  # type: float
   computed_at = sqlalchemy.Column(
-    sqlalchemy.DateTime, default=datetime.datetime.utcnow)  # type: datetime.datetime
+      sqlalchemy.DateTime,
+      default=datetime.datetime.utcnow)  # type: datetime.datetime
 
   def __repr__(self) -> Text:
     return "<Metric(name='%s', value='%.1f', computed_at='%s')>" % (
