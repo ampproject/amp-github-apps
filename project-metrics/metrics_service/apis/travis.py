c192ab4f74c2feda1347f7b9ed7e5499c2285a2a
Ryan Cebulko <ryan@cebulko.com>
2019-07-03 16:49:51 -0400

    Migrate Travis API from V2 REST API to V3 GraphQL API

--- project-metrics/metrics_service/apis/travis.py
+++ project-metrics/metrics_service/apis/travis.py
@@ -2,11 +2,13 @@
 
 from agithub import base as agithub_base
 from flask_api import status
+import datetime
 import functools
 import logging
-from typing import Any, Dict, Text
+from typing import Any, Dict, Sequence, Text
 
 import env
+from database import models
 
 
 class TravisApiError(Exception):
@@ -26,13 +28,15 @@ class TravisApiError(Exception):
 class TravisApi(agithub_base.API):
   """Travis API interface."""
 
-  def __init__(self, *args, **kwargs):
+  # Default/maximum page size from the API.
+  TRAVIS_PAGE_SIZE = 25
 
+  def __init__(self, *args, **kwargs):
     extra_headers = {
         'User-Agent': 'AMPProjectMetrics/1.0.0',
-        'Accept': 'application/vnd.travis-ci.2.1+json',
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
     }
+
     props = agithub_base.ConnectionProperties(
         api_url='api.travis-ci.org',
         secure_http=True,
@@ -40,8 +44,20 @@ class TravisApi(agithub_base.API):
     self.setClient(agithub_base.Client(*args, **kwargs))
     self.setConnectionProperties(props)
 
-    self._token = self._get_travis_token(env.get('GITHUB_API_ACCESS_TOKEN'))
+    try:
+      self.api_token = env.get('TRAVIS_API_ACCESS_TOKEN')
+    except KeyError:
+      logging.warn('No Travis API key found; exchanging GitHub token')
+      self.api_token = self._get_travis_token(
+          env.get('GITHUB_API_ACCESS_TOKEN'))
+
     extra_headers['Authorization'] = 'token %s' % self._token
+    extra_headers['Travis-API-Version'] = '3'
+    props = agithub_base.ConnectionProperties(
+        api_url='api.travis-ci.org',
+        secure_http=True,
+        extra_headers=extra_headers)
+    self.setConnectionProperties(props)
 
   @functools.lru_cache()
   def _get_travis_token(self, github_token: Text) -> Text:
@@ -69,13 +85,25 @@ class TravisApi(agithub_base.API):
   @property
   def repo(self) -> agithub_base.IncompleteRequest:
     """Returns a partial Travis request for the repository in env.yaml."""
-    return self.repos[env.get('GITHUB_REPO')]
+    return self['repo'][env.get('GITHUB_REPO').replace('/', '%2F')]
 
-  def fetch_builds(self, after_number: int = None) -> Dict[Text, Any]:
+  def _json_to_build(self, json_build: Dict[Text, Any]) -> models.Build:
+    started_at = datetime.datetime.strptime(
+        json_build['started_at'],
+        '%Y-%m-%dT%H:%M:%SZ') if json_build['started_at'] else None
+    return models.Build(
+        id=json_build['id'],
+        number=int(json_build['number']),
+        duration=json_build['duration'],
+        state=models.TravisState(json_build['state']),
+        started_at=started_at,
+        commit_hash=json_build['commit']['sha'])
+
+  def fetch_builds(self, page_num: int = 0) -> Sequence[models.Build]:
     """Fetches pull request builds from Travis.
 
     Args:
-      after_number: build number to start from (for paging).
+      page_num: zero-indexed page number to fetch.
 
     Raises:
       TravisApiError: if the call to the Travis Builds API fails.
@@ -84,12 +112,17 @@ class TravisApi(agithub_base.API):
       The API response containing a `builds` list and associated `commits` list.
       See https://docs.travis-ci.com/api/#builds
     """
-    params = {'event_type': 'pull_request'}
-    if after_number:
-      params['after_number'] = after_number
+    params = {
+        'event_type': 'pull_request',
+        'sort_by': 'id:desc',
+        'offset': page_num * self.TRAVIS_PAGE_SIZE,
+        'limit': self.TRAVIS_PAGE_SIZE,
+        'branch.name': 'master',
+    }
     status_code, response = self.repo.builds.get(**params)
+
     if status_code == status.HTTP_200_OK:
-      return response
+      return [self._json_to_build(build) for build in response['builds']]
     raise TravisApiError(
         status_code,
         'Travis Builds API request failed with response: %s' % response)
--- project-metrics/metrics_service/apis/travis_test.py
+++ project-metrics/metrics_service/apis/travis_test.py
@@ -1,5 +1,8 @@
 """Tests for Travis API interface."""
 
+import datetime
+import sqlalchemy
+import sqlalchemy.testing
 import unittest
 from unittest import mock
 from agithub import base as agithub_base
@@ -14,13 +17,33 @@ class TestTravisApi(unittest.TestCase):
   def setUp(self):
     super(TestTravisApi, self).setUp()
 
+    self.session = sqlalchemy.orm.create_session(bind=sqlalchemy.testing.db)
+    self.build = models.Build(
+        id=15492917,
+        number=1,
+        state=models.TravisState.FAILED,
+        duration=8,
+        started_at=datetime.datetime(2015, 9, 12, 19, 19, 25),
+        commit_hash='5589a4d3d3fc008c77026f5e7f996a61200298bb',
+    )
+    self.build_json = {
+        'id': 15492917,
+        'number': '1',
+        'state': 'failed',
+        'duration': 8,
+        'started_at': '2015-09-12T19:19:25Z',
+        'commit': {
+            'sha': '5589a4d3d3fc008c77026f5e7f996a61200298bb',
+        },
+    }
+
     mock.patch.object(
         env,
         'get',
         side_effect={
             'GITHUB_REPO': '__repo__',
             'GITHUB_API_ACCESS_TOKEN': '__github_token__',
-        }.get).start()
+        }.__getitem__).start()
     self.mock_request = mock.patch.object(
         agithub_base.Client, 'request', autospec=True).start()
 
@@ -31,7 +54,7 @@ class TestTravisApi(unittest.TestCase):
 
     travis_api = travis.TravisApi()
 
-    self.assertEqual(travis_api._token, '__travis_token__')
+    self.assertEqual(travis_api.api_token, '__travis_token__')
     self.mock_request.assert_called_once_with(
         travis_api.client, 'POST', '/auth/github?github_token=__github_token__',
         mock.ANY, mock.ANY)
@@ -42,38 +65,42 @@ class TestTravisApi(unittest.TestCase):
     with self.assertRaisesRegex(
         travis.TravisApiError,
         (r'Travis API Exception \(HTTP 403\): '
-          'Travis Auth API request failed with response: Unauthorized\.')
-    ):
+         r'Travis Auth API request failed with response: Unauthorized\.')):
       travis.TravisApi()
 
   @mock.patch.object(
       travis.TravisApi, '_get_travis_token', return_value='__travis_token__')
   def testFetchBuilds(self, unused_mock_get_travis_token):
-    build = models.Build(duration=1000, state=models.TravisState.PASSED)
-    self.mock_request.return_value = (200, [build])
+    self.mock_request.return_value = (200, {'builds': [self.build_json]})
 
     travis_api = travis.TravisApi()
     fetched_builds = travis_api.fetch_builds()
 
-    self.assertEqual(fetched_builds, [build])
-    self.mock_request.assert_called_once_with(
-        travis_api.client, 'GET',
-        '/repos/__repo__/builds?event_type=pull_request', mock.ANY, mock.ANY)
+    # Comparing the Build directly fails due to internal state properties used
+    # by SQLAlchemy.
+    self.assertEqual(fetched_builds[0].started_at, self.build.started_at)
+    self.assertEqual(fetched_builds[0].id, self.build.id)
+    self.assertEqual(fetched_builds[0].number, self.build.number)
+    self.assertEqual(fetched_builds[0].duration, self.build.duration)
+    self.assertEqual(fetched_builds[0].commit_hash, self.build.commit_hash)
+    self.assertEqual(fetched_builds[0].state, self.build.state)
 
   @mock.patch.object(
       travis.TravisApi, '_get_travis_token', return_value='__travis_token__')
-  def testFetchBuildsAfterNumber(self, unused_mock_get_travis_token):
-    build = models.Build(duration=1000, state=models.TravisState.PASSED)
-    self.mock_request.return_value = (200, [build])
+  def testFetchBuildsPage(self, unused_mock_get_travis_token):
+    self.mock_request.return_value = (200, {'builds': [self.build_json]})
 
     travis_api = travis.TravisApi()
-    fetched_builds = travis_api.fetch_builds(after_number=3)
+    fetched_builds = travis_api.fetch_builds(page_num=1)
 
-    self.assertEqual(fetched_builds, [build])
-    self.mock_request.assert_called_once_with(
-        travis_api.client, 'GET',
-        '/repos/__repo__/builds?event_type=pull_request&after_number=3',
-        mock.ANY, mock.ANY)
+    # Comparing the Build directly fails due to internal state properties used
+    # by SQLAlchemy.
+    self.assertEqual(fetched_builds[0].started_at, self.build.started_at)
+    self.assertEqual(fetched_builds[0].id, self.build.id)
+    self.assertEqual(fetched_builds[0].number, self.build.number)
+    self.assertEqual(fetched_builds[0].duration, self.build.duration)
+    self.assertEqual(fetched_builds[0].commit_hash, self.build.commit_hash)
+    self.assertEqual(fetched_builds[0].state, self.build.state)
 
   @mock.patch.object(
       travis.TravisApi, '_get_travis_token', return_value='__travis_token__')
@@ -84,6 +111,5 @@ class TestTravisApi(unittest.TestCase):
     with self.assertRaisesRegex(
         travis.TravisApiError,
         (r'Travis API Exception \(HTTP 500\): '
-          'Travis Builds API request failed with response: Server error\.')
-    ):
-      travis_api.fetch_builds(after_number=3)
+         r'Travis Builds API request failed with response: Server error\.')):
+      travis_api.fetch_builds()
