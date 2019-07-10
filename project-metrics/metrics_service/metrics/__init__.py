30fea7f529d72182f53eef27c23a6bb02b877102
Ryan Cebulko <ryan@cebulko.com>
2019-07-10 16:24:37 -0400

    Fix imports and serialization for metrics

--- project-metrics/metrics_service/metrics/__init__.py
+++ project-metrics/metrics_service/metrics/__init__.py
@@ -1,3 +1,5 @@
 """Import metric implementations so they can register themselves."""
-from . import base
-from . import example
+from metrics import base
+from metrics import absolute_coverage
+from metrics import presubmit_latency
+from metrics import travis_greenness
