a381f068bbef3f8607603ffc0a8bac0dceab5818
Ryan Cebulko <ryan@cebulko.com>
2019-07-18 15:07:28 -0400

    Project Metrics: Implement release-cherrypick-count metric and tests (#256)
    * Project Metrics: Implement release-cherrypick-count metric and tests
    
    * Code review fixes

---
+++ project-metrics/metrics_service/metrics/release_cherrypick_count.py
@@ -0,0 +1,43 @@
+"""Release Cherrypick Count metric."""
+
+import logging
+import sqlalchemy
+from typing import Text, Dict
+
+from database import db
+from database import models
+from metrics import base
+
+
+class ReleaseCherrypickCountMetric(base.Metric):
+  """A metric tracking the number of cherry-picks in releases."""
+
+  def _format_value(self, num_cherrypicks: float) -> Text:
+    return '%d PR%s/90d' % (num_cherrypicks,
+                            ('s' if num_cherrypicks > 1 else ''))
+
+  def _score_value(self, num_cherrypicks: float) -> models.MetricScore:
+    if num_cherrypicks > 10:
+      return models.MetricScore.CRITICAL
+    elif num_cherrypicks > 5:
+      return models.MetricScore.POOR
+    elif num_cherrypicks > 3:
+      return models.MetricScore.MODERATE
+    elif num_cherrypicks > 1:
+      return models.MetricScore.GOOD
+    else:
+      return models.MetricScore.EXCELLENT
+
+  def _compute_value(self) -> float:
+    """Counts the number of cherry-picks in the last 90 days.
+
+    Returns:
+      The number of cherry-picks.
+    """
+    logging.info('Counting cherry-picks')
+    session = db.Session()
+    return session.query(models.Cherrypick).join(models.Release).filter(
+        models.Release.is_last_90_days()).count()
+
+
+base.Metric.register(ReleaseCherrypickCountMetric)
