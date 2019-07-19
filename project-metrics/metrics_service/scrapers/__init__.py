5224fdde0a392b5fc0f0ab926eecc2b6ad90139b
Ryan Cebulko <ryan@cebulko.com>
2019-07-19 13:33:58 -0400

    Add historical scraping to scrapers module

--- project-metrics/metrics_service/scrapers/__init__.py
+++ project-metrics/metrics_service/scrapers/__init__.py
@@ -1,9 +1,14 @@
+import datetime
+from typing import Optional, Text
+
 from scrapers.commit_scraper import CommitScraper
 from scrapers.build_scraper import BuildScraper
 from scrapers.release_scraper import ReleaseScraper
 from scrapers.cherrypick_scraper import CherrypickScraper
 
-_scraper_map = {
+NINETY_DAYS = datetime.timedelta(days=90)
+ONE_YEAR = datetime.timedelta(days=365)
+SCRAPER_MAP = {
     'commits': CommitScraper,
     'builds': BuildScraper,
     'releases': ReleaseScraper,
@@ -11,5 +16,30 @@ _scraper_map = {
 }
 
 
-def scrape(scrape_target: str):
-  _scraper_map[scrape_target].scrape()
+def scrape(scrape_target: Text):
+  """Scrape latest data to populate the DB.
+
+  Args:
+    scrape_target: type of data to scrape.
+
+  Raises:
+    KeyError: if an invalid scrape target is passed
+  """
+  SCRAPER_MAP[scrape_target].scrape()
+
+
+def scrape_historical(scrape_target: Text,
+                      since: Optional[datetime.datetime] = None):
+  """Scrape historical data to backfill DB for historical metric computation.
+
+  Args:
+    scrape_target: type of data to scrape.
+    since: datetime to scrape back until; defaults to one year and ninety days
+      ago
+
+  Raises:
+    KeyError: if an invalid scrape target is passed
+  """
+  if since is None:
+    since = datetime.datetime.now() - (ONE_YEAR + NINETY_DAYS)
+  SCRAPER_MAP[scrape_target]().scrape_historical(since)
