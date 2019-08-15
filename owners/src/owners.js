e0dee2d6f784fb2c09c16c30e297035f52e6cd53
Ryan Cebulko <ryan@cebulko.com>
2019-08-15 13:56:38 -0400

    Add class to manage parsing OWNERS files

---
+++ owners/src/owners.js
@@ -0,0 +1,106 @@
+/**
+ * Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const path = require('path');
+const yaml = require('yamljs');
+
+
+/**
+ * A rule describing ownership for a directory.
+ */
+class OwnersRule {
+  /**
+   * Constructor.
+   *
+   * @param {!string} ownersPath path to OWNERS file.
+   * @param {!string[]} owners list of GitHub usernames of owners.
+   */
+  constructor(ownersPath, owners) {
+    this.dirPath = path.dirname(ownersPath);
+    this.owners = owners;
+  }
+
+  /**
+   * Determines the directory tree depth of the OWNERS file.
+   *
+   * Used to determine precedence. The root OWNERS file has a depth of 0.
+   *
+   * @return {number} tree depth of the OWNERS file.
+   */
+  get depth() {
+    return this.dirPath === '.' ? 0 : this.dirPath.split(path.sep).length;
+  }
+
+  /**
+   * Test if a file is matched by the rule.
+   *
+   * @param {!string} filePath relative path in repo to the file being checked.
+   * @return {boolean} true if the rule applies to the file.
+   */
+  matchesFile(filePath) {
+    const filePathDir = path.dirname(filePath)
+    const filePathSegments =
+        filePathDir.split(path.sep).filter(segment => segment != '.');
+    const rulePathSegments =
+        this.dirPath.split(path.sep).filter(segment => segment != '.');
+
+    if (filePathSegments.length < rulePathSegments.length) {
+      return false;
+    }
+
+    for (let i = 0; i < rulePathSegments.length; ++i) {
+      if (rulePathSegments[i] !== filePathSegments[i]) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+
+/**
+ * Parser for OWNERS.yaml files.
+ */
+class OwnersParser {
+  /**
+   * Constructor.
+   *
+   * @param {!LocalRepository} localRepo local repository to read from.
+   */
+  constructor(localRepo) {
+    this.localRepo = localRepo;
+  }
+
+  /**
+   * Parse an OWNERS.yaml file.
+   *
+   * @param {!string} ownersPath OWNERS.yaml file path.
+   * @return {OwnersRule} parsed OWNERS file rule.
+   */
+  async parseOwnersFile(ownersPath) {
+    const contents = this.localRepo.readFile(ownersPath);
+    const ownersList = yaml.parse(contents);
+
+    return new OwnersRule(ownersPath, ownersList);
+  }
+}
+
+
+module.exports = {
+  OwnersParser,
+  OwnersRule
+};
---
+++ owners/test/owners.test.js
@@ -0,0 +1,116 @@
+/**
+ * Copyright 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const {LocalRepository} = require('../src/local_repo');
+const sinon = require('sinon');
+const {OwnersParser, OwnersRule} = require('../src/owners');
+
+describe('owners rules', () => {
+  describe('depth', () => {
+    it('should return 0 for files at the root', () => {
+      const rule = new OwnersRule('OWNERS.yaml', []);
+      expect(rule.depth).toEqual(0);
+    });
+
+
+    it('should return tree depth for files not at the root', () => {
+      const ruleOne = new OwnersRule('src/OWNERS.yaml', []);
+      const ruleTwo = new OwnersRule('foo/bar/OWNERS.yaml', []);
+      const ruleFive = new OwnersRule('foo/bar/baz/biz/buzz/OWNERS.yaml', []);
+
+      expect(ruleOne.depth).toEqual(1);
+      expect(ruleTwo.depth).toEqual(2);
+      expect(ruleFive.depth).toEqual(5);
+    });
+  });
+
+  describe('matchesFile', () => {
+    expect.extend({
+      toMatchFile(receivedOwnersPath, filePath) {
+        const rule = new OwnersRule(receivedOwnersPath, []);
+        const matches = rule.matchesFile(filePath);
+        const matchStr = this.isNot ? 'not match' : 'match'
+
+        return {
+          pass: matches,
+          message: () => `Expected rules in '${receivedOwnersPath}' to ` +
+              `${matchStr} file '${filePath}'.`,
+        };
+      }
+    });
+
+    it('matches a file in the same directory', () => {
+      expect('OWNERS.yaml').toMatchFile('foo.txt');
+    });
+
+    it('matches a file in a child directory', () => {
+      expect('OWNERS.yaml').toMatchFile('foo/bar.txt');
+    });
+
+    it('matches a file in an ancestor directory', () => {
+      expect('OWNERS.yaml').toMatchFile('foo/bar/baz.txt');
+    });
+
+    it('does not match a file in a parent directory', () => {
+      expect('src/OWNERS.yaml').not.toMatchFile('foo.txt');
+    });
+
+    it('does not match a file in a sibling directory', () => {
+      expect('src/OWNERS.yaml').not.toMatchFile('test/foo.txt');
+    });
+  });
+});
+
+describe('owners parser', () => {
+  const sandbox = sinon.createSandbox();
+  let repo;
+  let parser;
+
+  beforeEach(() => {
+    repo = new LocalRepository('path/to/repo');
+    parser = new OwnersParser(repo);
+  });
+
+  afterEach(() => {
+    sandbox.restore()
+  });
+
+  describe('parseOwnersFile', () => {
+    it('reads the file from the local repository', () => {
+      sandbox.stub(repo, 'readFile').returns('');
+      parser.parseOwnersFile('foo/OWNERS.yaml');
+      sandbox.assert.calledWith(repo.readFile, 'foo/OWNERS.yaml');
+    });
+
+    it('assigns the OWNERS directory path', async () => {
+      sandbox.stub(repo, 'readFile').returns('');
+      const rule = await parser.parseOwnersFile('foo/OWNERS.yaml');
+      expect(rule.dirPath).toEqual('foo');
+    });
+
+    it('parses a YAML list', async () => {
+      sandbox.stub(repo, 'readFile').returns('- user1\n- user2\n');
+      const rule = await parser.parseOwnersFile('');
+      expect(rule.owners).toEqual(['user1', 'user2']);
+    });
+
+    it('parses a YAML list with blank lines and comments', async () => {
+      sandbox.stub(repo, 'readFile').returns('- user1\n# comment\n\n- user2\n');
+      const rule = await parser.parseOwnersFile('');
+      expect(rule.owners).toEqual(['user1', 'user2']);
+    });
+  });
+});
