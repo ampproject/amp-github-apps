d6338d851bcecfd2be33d43442d91b1b701f76c7
Ryan Cebulko <ryan@cebulko.com>
2019-09-04 15:51:51 -0400

    [owners] Add reviewer selection algorithm  (#379)
    * Add test stubs
    
    * Add tests
    
    * Lint fixes
    
    * Fix error handling
    
    * Remove old selector file with pseudocode
    
    * Clean up comments
    
    * Code review fixes
    
    * Wrap lambda in braces

---
+++ owners/src/reviewer_selection.js
@@ -0,0 +1,188 @@
+/**
+ * Copyright 2019 The AMP HTML Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+
+=== Reviewer Selection Algorithm ===
+
+Inputs: a map from files needing approval to the nearest ownership subtree
+
+1. Select the set of ownership rules at the greatest depth in the tree (referred
+   to as "deepest rules" going forward) and union all owners; this is the set of
+   potential reviewers.
+2. For each potential reviewer, count the number of files they can approve, and
+   select whichever potential reviewer could cover the most additional files.
+3. Remove all files from the set that can be approved by the new reviewer, and
+   all ownership rules which are fully satisfied/which contain the new reviewer.
+
+Repeat until all files have an OWNER in the reviewer set.
+
+*/
+
+/**
+ * Class implementing the reviewer selection algorithm.
+ */
+class ReviewerSelection {
+  /** Part 1 **/
+
+  /**
+   * Produce a set of all ownership subtrees directly owning a changed file.
+   *
+   * @private
+   * @param {!FileTreeMap} fileTreeMap map from filenames to ownership subtrees.
+   * @return {Set<OwnersTree>} a set of the nearest ownership subtrees.
+   */
+  static _nearestOwnersTrees(fileTreeMap) {
+    const trees = new Set(Object.values(fileTreeMap));
+    return Array.from(trees);
+  }
+
+  /**
+   * Produce the set of all owners for a set of ownership trees.
+   *
+   * @private
+   * @param {OwnersTree[]} trees list of ownership trees.
+   * @return {string[]} union of reviewers lists for all trees.
+   */
+  static _reviewersForTrees(trees) {
+    const reviewers = new Set();
+    trees.forEach(tree => {
+      tree.rules.forEach(rule => {
+        rule.owners.forEach(owner => reviewers.add(owner));
+      });
+    });
+    return Array.from(reviewers);
+  }
+
+  /**
+   * Produce a list of reviewers to satisfy at least one of the deepest
+   * ownership rules.
+   *
+   * @private
+   * @param {!FileTreeMap} fileTreeMap map from filenames to ownership subtrees.
+   * @return {string[]} list of reviewer usernames.
+   */
+  static _findPotentialReviewers(fileTreeMap) {
+    const nearestTrees = this._nearestOwnersTrees(fileTreeMap);
+    const maxDepth = Math.max(...nearestTrees.map(tree => tree.depth));
+    const deepestTrees = nearestTrees.filter(tree => tree.depth === maxDepth);
+    return this._reviewersForTrees(deepestTrees);
+  }
+
+  /** Part 2 **/
+
+  /**
+   * List files owned by a reviewer.
+   *
+   * @private
+   * @param {!FileTreeMap} fileTreeMap map from filenames to ownership subtrees.
+   * @param {!string} reviewer reviewer username.
+   * @return {string[]} list of filenames.
+   */
+  static _filesOwnedByReviewer(fileTreeMap, reviewer) {
+    return Object.entries(fileTreeMap)
+      .filter(([filename, tree]) => tree.hasOwner(reviewer))
+      .map(([filename, tree]) => filename);
+  }
+
+  /**
+   * Identifies the reviewer(s) who can approve the most files.
+   *
+   * @private
+   * @param {!Object<string, string[]>} reviewerFilesMap map from reviewer
+   *     usernames to the list of files they own.
+   * @return {string[]} list of reviewer usernames.
+   */
+  static _reviewersWithMostFiles(reviewerFilesMap) {
+    const mostFilesOwned = Math.max(
+      ...Object.values(reviewerFilesMap).map(files => files.length)
+    );
+    return Object.entries(reviewerFilesMap).filter(
+      ([reviewer, files]) => files.length == mostFilesOwned
+    );
+  }
+
+  /**
+   * Picks the best reviewer-fileset pair to satisfy the deepest rules.
+   *
+   * @private
+   * Chooses randomly from the set of best possible reviewers to allow even
+   * distribution of reviews.
+   *
+   * @param {!FileTreeMap} fileTreeMap map from filenames to ownership subtrees.
+   * @return {ReviewerFiles} tuple of a reviewer username and the files they
+   *     own.
+   */
+  static _pickBestReview(fileTreeMap) {
+    const reviewerSet = this._findPotentialReviewers(fileTreeMap);
+    const reviewerFilesMap = {};
+    reviewerSet.forEach(reviewer => {
+      reviewerFilesMap[reviewer] = this._filesOwnedByReviewer(
+        fileTreeMap,
+        reviewer
+      );
+    });
+    const bestReviewers = this._reviewersWithMostFiles(reviewerFilesMap);
+    return bestReviewers[Math.floor(Math.random() * bestReviewers.length)];
+  }
+
+  /** Part 3 **/
+
+  /**
+   * Builds the map from filenames to ownership subtrees.
+   *
+   * @param {string[]} filenames list of changed files.
+   * @param {!OwnersTree} ownersTree root node of the ownership tree.
+   * @return {FileTreeMap} map from filenames to nearest ownership subtrees.
+   */
+  static buildFileTreeMap(filenames, ownersTree) {
+    const fileTreeMap = {};
+    filenames.forEach(filename => {
+      fileTreeMap[filename] = ownersTree.atPath(filename);
+    });
+    return fileTreeMap;
+  }
+
+  /**
+   * Picks a set of reviews to approve the PR.
+   *
+   * @throws {Error} if the algorithm fails to select reviewers.
+   * @param {!FileTreeMap} fileTreeMap map from filenames to ownership subtrees.
+   * @return {ReviewerFiles[]} list of reviewers and the files they cover, in
+   *     decreasing order of ownership depth.
+   */
+  static pickReviews(fileTreeMap) {
+    const reviews = [];
+
+    while (Object.entries(fileTreeMap).length) {
+      const bestReview = this._pickBestReview(fileTreeMap);
+      if (!bestReview) {
+        // This should be impossible unless there is no top-level OWNERS file.
+        throw Error('Could not select reviewers!');
+      }
+
+      const [bestReviewer, coveredFiles] = bestReview;
+      reviews.push([bestReviewer, coveredFiles]);
+      coveredFiles.forEach(filename => {
+        delete fileTreeMap[filename];
+      });
+    }
+
+    return reviews;
+  }
+}
+
+module.exports = {ReviewerSelection};
---
+++ owners/test/reviewer_selection.test.js
@@ -0,0 +1,266 @@
+/**
+ * Copyright 2019 The AMP HTML Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const sinon = require('sinon');
+const {ReviewerSelection} = require('../src/reviewer_selection');
+const {OwnersRule, OwnersTree} = require('../src/owners');
+
+describe('reviewer selection', () => {
+  const sandbox = sinon.createSandbox();
+  const ownersTree = new OwnersTree();
+
+  const dirTrees = {
+    '/': ownersTree.addRule(new OwnersRule('OWNERS.yaml', ['rootOwner'])),
+    '/foo': ownersTree.addRule(new OwnersRule('foo/OWNERS.yaml', ['child'])),
+    '/biz': ownersTree.addRule(
+      new OwnersRule('biz/OWNERS.yaml', ['child', 'kid'])
+    ),
+    '/buzz': ownersTree.addRule(
+      new OwnersRule('buzz/OWNERS.yaml', ['thirdChild'])
+    ),
+    '/foo/bar/baz': ownersTree.addRule(
+      new OwnersRule('foo/bar/baz/OWNERS.yaml', ['descendant'])
+    ),
+  };
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  describe('nearestOwnersTrees', () => {
+    it('returns the nearest trees', () => {
+      const fileTreeMap = ReviewerSelection.buildFileTreeMap(
+        ['./main.js', 'biz/style.css', 'foo/bar/other_file.js'],
+        ownersTree
+      );
+      const nearestTrees = ReviewerSelection._nearestOwnersTrees(fileTreeMap);
+
+      expect(nearestTrees).toEqual(
+        expect.arrayContaining([
+          dirTrees['/'],
+          dirTrees['/biz'],
+          dirTrees['/foo'],
+        ])
+      );
+    });
+
+    it('does not return duplicates', () => {
+      const fileTreeMap = ReviewerSelection.buildFileTreeMap(
+        ['foo/file.js', 'foo/bar/other_file.js'],
+        ownersTree
+      );
+      const nearestTrees = ReviewerSelection._nearestOwnersTrees(fileTreeMap);
+
+      expect(nearestTrees).toEqual([dirTrees['/foo']]);
+    });
+  });
+
+  describe('reviewersForTrees', () => {
+    it('unions the owners for all subtrees', () => {
+      const reviewers = ReviewerSelection._reviewersForTrees([
+        dirTrees['/biz'],
+        dirTrees['/foo/bar/baz'],
+      ]);
+
+      expect(reviewers).toEqual(
+        expect.arrayContaining(['child', 'kid', 'descendant'])
+      );
+    });
+
+    it('does not include inherited owners', () => {
+      const reviewers = ReviewerSelection._reviewersForTrees([
+        dirTrees['/foo'],
+      ]);
+
+      expect(reviewers).not.toContain('rootOwner');
+    });
+  });
+
+  describe('findPotentialReviewers', () => {
+    it('returns reviewers for the deepest trees', () => {
+      const fileTreeMap = ReviewerSelection.buildFileTreeMap(
+        ['./main.js', 'biz/style.css', 'foo/bar/other_file.js'],
+        ownersTree
+      );
+      sandbox.stub(ReviewerSelection, '_reviewersForTrees').callThrough();
+      const reviewers = ReviewerSelection._findPotentialReviewers(fileTreeMap);
+
+      sandbox.assert.calledWith(
+        ReviewerSelection._reviewersForTrees,
+        sinon.match.array.contains([dirTrees['/foo'], dirTrees['/biz']])
+      );
+      expect(reviewers).toEqual(expect.arrayContaining(['child', 'kid']));
+    });
+  });
+
+  describe('filesOwnedByReviewer', () => {
+    const fileTreeMap = ReviewerSelection.buildFileTreeMap(
+      [
+        './main.js', // root
+        'biz/style.css', // child, kid, root
+        'foo/bar/other_file.js', // child, root
+        'foo/bar/baz/README.md', // descendant, child, root
+      ],
+      ownersTree
+    );
+
+    const filesOwnedMap = {
+      'rootOwner': [
+        './main.js',
+        'biz/style.css',
+        'foo/bar/other_file.js',
+        'foo/bar/baz/README.md',
+      ],
+      'child': [
+        'biz/style.css',
+        'foo/bar/other_file.js',
+        'foo/bar/baz/README.md',
+      ],
+      'kid': ['biz/style.css'],
+      'descendant': ['foo/bar/baz/README.md'],
+    };
+
+    it.each(Object.entries(filesOwnedMap))(
+      'lists files for %p',
+      (reviewer, expectedFilesOwned) => {
+        const filesOwned = ReviewerSelection._filesOwnedByReviewer(
+          fileTreeMap,
+          reviewer
+        );
+
+        expect(filesOwned).toEqual(expectedFilesOwned);
+      }
+    );
+  });
+
+  describe('reviewersWithMostFiles', () => {
+    it('lists the reviewer(s) who own the most files', () => {
+      const reviewerFiles = ReviewerSelection._reviewersWithMostFiles({
+        child: ['foo/file.js', 'foo/bar/other_file.js'],
+        kid: ['biz/style.css'],
+        thirdChild: ['buzz/info.txt', 'buzz/code.js'],
+      });
+
+      expect(reviewerFiles).toEqual(
+        expect.arrayContaining([
+          ['child', ['foo/file.js', 'foo/bar/other_file.js']],
+          ['thirdChild', ['buzz/info.txt', 'buzz/code.js']],
+        ])
+      );
+    });
+  });
+
+  describe('pickBestReview', () => {
+    const fileTreeMap = ReviewerSelection.buildFileTreeMap(
+      [
+        './main.js', // root
+        'biz/style.css', // child, kid, root
+        'foo/bar/other_file.js', // child, root
+        'buzz/info.txt', // thirdChild, root
+        'buzz/code.js', // thirdChild, root
+      ],
+      ownersTree
+    );
+
+    it('builds a map from deepest reviewers to files they own', () => {
+      sandbox.stub(ReviewerSelection, '_reviewersWithMostFiles').callThrough();
+      ReviewerSelection._pickBestReview(fileTreeMap);
+
+      sandbox.assert.calledWith(ReviewerSelection._reviewersWithMostFiles, {
+        child: ['biz/style.css', 'foo/bar/other_file.js'],
+        kid: ['biz/style.css'],
+        thirdChild: ['buzz/info.txt', 'buzz/code.js'],
+      });
+    });
+
+    it('picks one of the reviewers with the most files', () => {
+      const [bestReviewer, filesCovered] = ReviewerSelection._pickBestReview(
+        fileTreeMap
+      );
+
+      expect(['child', 'thirdChild']).toContain(bestReviewer);
+      expect(filesCovered.length).toEqual(2);
+    });
+  });
+
+  describe('buildFileTreeMap', () => {
+    it('builds a map from filenames to subtrees', () => {
+      const fileTreeMap = ReviewerSelection.buildFileTreeMap(
+        [
+          './main.js',
+          './package.json',
+          'biz/style.css',
+          'foo/file.js',
+          'foo/bar/other_file.js',
+          'buzz/info.txt',
+          'buzz/code.js',
+        ],
+        ownersTree
+      );
+
+      expect(fileTreeMap).toEqual({
+        './main.js': dirTrees['/'],
+        './package.json': dirTrees['/'],
+        'biz/style.css': dirTrees['/biz'],
+        'foo/file.js': dirTrees['/foo'],
+        'foo/bar/other_file.js': dirTrees['/foo'],
+        'buzz/info.txt': dirTrees['/buzz'],
+        'buzz/code.js': dirTrees['/buzz'],
+      });
+    });
+  });
+
+  describe('pickReviews', () => {
+    it('picks reviewers until all files are covered', () => {
+      sandbox.stub(ReviewerSelection, '_pickBestReview').callThrough();
+      const fileTreeMap = ReviewerSelection.buildFileTreeMap(
+        ['./main.js', 'foo/file.js', 'biz/style.css', 'buzz/info.txt'],
+        ownersTree
+      );
+
+      const reviews = ReviewerSelection.pickReviews(fileTreeMap);
+      const reviewers = reviews.map(([reviewer, files]) => reviewer);
+
+      sandbox.assert.calledThrice(ReviewerSelection._pickBestReview);
+      expect(reviewers).toEqual(['child', 'thirdChild', 'rootOwner']);
+    });
+
+    it('can avoid adding high-level owners', () => {
+      const fileTreeMap = ReviewerSelection.buildFileTreeMap(
+        ['foo/file.js', 'biz/style.css', 'buzz/info.txt'],
+        ownersTree
+      );
+
+      const reviews = ReviewerSelection.pickReviews(fileTreeMap);
+      const reviewers = reviews.map(([reviewer, files]) => reviewer);
+
+      expect(reviewers).toContain('child', 'thirdChild');
+      expect(reviewers).not.toContain('rootOwner');
+    });
+
+    it('throws an error if it fails to find reviewer coverage', () => {
+      sandbox.stub(ReviewerSelection, '_pickBestReview').returns(undefined);
+      const fileTreeMap = ReviewerSelection.buildFileTreeMap(
+        ['main.js'],
+        ownersTree
+      );
+
+      expect(() => ReviewerSelection.pickReviews(fileTreeMap)).toThrow(
+        'Could not select reviewers!'
+      );
+    });
+  });
+});
