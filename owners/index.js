e999f8c65f33c126b8d813d8a2c1777594522d36
Ryan Cebulko <ryan@cebulko.com>
2019-09-06 11:15:53 -0400

    The one-day cache was a silly idea

--- owners/index.js
+++ owners/index.js
@@ -26,7 +26,6 @@ const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 module.exports = app => {
   const localRepo = new LocalRepository(process.env.GITHUB_REPO_DIR);
   const ownersBot = new OwnersBot(localRepo);
-  const treeCache = new OneDayCache();
   const router = app.route('/admin');
 
   // Probot does not stream properly to GCE logs so we need to hook into
@@ -43,10 +42,8 @@ module.exports = app => {
   });
 
   router.get('/tree', async (req, res) => {
-    const ownersTree = await treeCache.wrapAsync(async () => {
-      const parser = new OwnersParser(localRepo, req.log);
-      return await parser.parseOwnersTree();
-    });
+    const parser = new OwnersParser(localRepo, req.log);
+    const ownersTree = await parser.parseOwnersTree();
 
     res.send(`<pre>${ownersTree.toString()}</pre>`);
   });
@@ -57,6 +54,7 @@ module.exports = app => {
     const pr = await github.getPullRequest(req.params.prNumber);
     const ownersCheck = new OwnersCheck(localRepo, github, pr);
     const checkRun = await ownersCheck.run();
+
     res.send(checkRun.json);
   });
 
@@ -88,45 +86,3 @@ module.exports = app => {
     );
   });
 };
-
-/**
- * Simple one-day cache for endpoints that do Git commands and file reads.
- *
- * TODO(rcebulko): Replace with a real caching solution once the health-check
- * server gets real functionality.
- */
-class OneDayCache {
-  /**
-   * Constructor.
-   */
-  constructor() {
-    this.lastUpdate = this.yesterday;
-    this.lastValue = null;
-  }
-
-  /**
-   * Get the date 24 hours ago.
-   *
-   * @return {Date} yesterday's date.
-   */
-  get yesterday() {
-    const date = new Date();
-    date.setDate(date.getDate() - 1);
-    return date;
-  }
-
-  /**
-   * Wraps an async function with the cache.
-   *
-   * @param {!function} asyncFn function to wrap.
-   * @return {any} computed or cached result.
-   */
-  async wrapAsync(asyncFn) {
-    if (this.lastValue === null || this.lastUpdate < this.yesterday) {
-      this.lastUpdate = new Date();
-      this.lastValue = await asyncFn();
-    }
-
-    return this.lastValue;
-  }
-}
