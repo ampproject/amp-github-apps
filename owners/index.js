721092ca22daf4cb392b1bf0a790416133c4cf74
Ryan Cebulko <ryan@cebulko.com>
2019-10-03 11:13:56 -0400

    [owners] Switch from ServerJS to Express for status server (#497)
    * Switch from ServerJS to Express for status server
    
    * Use exact version
    
    * Lint fixes

--- owners/index.js
+++ owners/index.js
@@ -20,7 +20,7 @@ const {LocalRepository} = require('./src/local_repo');
 const {OwnersBot} = require('./src/owners_bot');
 const {OwnersParser} = require('./src/parser');
 const {OwnersCheck} = require('./src/owners_check');
-const server = require('server');
+const express = require('express');
 
 const GITHUB_ACCESS_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
 const GITHUB_REPO = process.env.GITHUB_REPO || 'ampproject/amphtml';
@@ -30,6 +30,7 @@ const GCLOUD_PROJECT = process.env.GCLOUD_PROJECT || 'UNKNOWN';
 const APP_ID = process.env.APP_ID || 'UNKNOWN';
 const APP_COMMIT_SHA = process.env.APP_COMMIT_SHA || 'UNKNOWN';
 const APP_COMMIT_MSG = process.env.APP_COMMIT_MSG || 'UNKNOWN';
+const INFO_SERVER_PORT = Number(process.env.INFO_SERVER_PORT || 8081);
 
 const CACHED_TREE_REFRESH_MS = 10 * 60 * 1000;
 
@@ -107,13 +108,14 @@ module.exports = app => {
   }
 
   if (process.env.NODE_ENV !== 'test') {
+    updateTree();
     teamsInitialized.then(updateTree);
     setInterval(updateTree, CACHED_TREE_REFRESH_MS);
 
     /** Health check server endpoints **/
-    app.log(`Starting status server on port ${process.env.INFO_SERVER_PORT}`);
-    server({port: process.env.INFO_SERVER_PORT || 8081}, [
-      server.router.get('/status', ctx =>
+    const expressApp = express();
+    expressApp.get('/status', (req, res) => {
+      res.send(
         [
           `The OWNERS bot is live and running on ${GITHUB_REPO}!`,
           `Project: ${GCLOUD_PROJECT}`,
@@ -122,38 +124,42 @@ module.exports = app => {
           '<a href="/tree">Owners Tree</a>',
           '<a href="/teams">Organization Teams</a>',
         ].join('<br>')
-      ),
+      );
+    });
 
-      server.router.get('/tree', async ctx => {
-        const treeHeader = '<h3>OWNERS tree</h3>';
-        const treeDisplay = `<pre>${treeParse.result.toString()}</pre>`;
+    expressApp.get('/tree', (req, res) => {
+      const treeHeader = '<h3>OWNERS tree</h3>';
+      const treeDisplay = `<pre>${treeParse.result.toString()}</pre>`;
 
-        let output = `${treeHeader}${treeDisplay}`;
-        if (treeParse.errors.length) {
-          const errorHeader = '<h3>Parser Errors</h3>';
-          const errorDisplay = treeParse.errors
-            .map(error => error.toString())
-            .join('<br>');
-          output += `${errorHeader}<code>${errorDisplay}</code>`;
-        }
+      let output = `${treeHeader}${treeDisplay}`;
+      if (treeParse.errors.length) {
+        const errorHeader = '<h3>Parser Errors</h3>';
+        const errorDisplay = treeParse.errors
+          .map(error => error.toString())
+          .join('<br>');
+        output += `${errorHeader}<code>${errorDisplay}</code>`;
+      }
 
-        return output;
-      }),
+      res.send(output);
+    });
 
-      server.router.get('/teams', (req, res) => {
-        const teamSections = [];
-        Object.entries(ownersBot.teams).forEach(([name, team]) => {
-          teamSections.push(
-            [
-              `Team "${name}" (ID: ${team.id}):`,
-              ...team.members.map(username => `- ${username}`),
-            ].join('<br>')
-          );
-        });
+    expressApp.get('/teams', (req, res) => {
+      const teamSections = [];
+      Object.entries(ownersBot.teams).forEach(([name, team]) => {
+        teamSections.push(
+          [
+            `Team "${name}" (ID: ${team.id}):`,
+            ...team.members.map(username => `- ${username}`),
+          ].join('<br>')
+        );
+      });
 
-        return ['<h2>Teams</h2>', ...teamSections].join('<br><br>');
-      }),
-    ]);
+      res.send(['<h2>Teams</h2>', ...teamSections].join('<br><br>'));
+    });
+
+    expressApp.listen(INFO_SERVER_PORT, () => {
+      app.log(`Starting status server on port ${INFO_SERVER_PORT}`);
+    });
   }
 
   // Since this endpoint triggers a ton of GitHub API requests, there is a risk
