733964f88c88a94e3692749ef50ac80277899ede
Ryan Cebulko <ryan@cebulko.com>
2019-10-18 12:35:52 -0400

    Fix jsdoc comments in owners check

--- owners/src/owners_check.js
+++ owners/src/owners_check.js
@@ -46,7 +46,7 @@ class CheckRun {
   /**
    * Produces a JSON version of the object for use with GitHub API.
    *
-   * @return {object} JSON object describing check-run.
+   * @return {!object} JSON object describing check-run.
    */
   get json() {
     return {
@@ -74,7 +74,7 @@ class OwnersCheck {
    * Constructor.
    *
    * @param {!OwnersTree} tree file ownership tree.
-   * @param {string[]} changedFiles list of change files.
+   * @param {!Array<!string>} changedFiles list of change files.
    * @param {!ReviewerApprovalMap} reviewers map of reviewer approval statuses.
    */
   constructor(tree, changedFiles, reviewers) {
@@ -88,7 +88,7 @@ class OwnersCheck {
   /**
    * Runs the owners check and, if necessary, the reviewer selection algorithm.
    *
-   * @return {OwnersCheckResult} a GitHub check-run and reviewers to add.
+   * @return {!OwnersCheckResult} a GitHub check-run and reviewers to add.
    */
   run() {
     try {
@@ -171,8 +171,8 @@ class OwnersCheck {
    *
    * @param {!string} filename file to check.
    * @param {!OwnersTree} subtree nearest ownership tree to file.
-   * @param {boolean} isApproved approval status to filter by.
-   * @return {boolean} if the file is reviewed.
+   * @param {!boolean} isApproved approval status to filter by.
+   * @return {!boolean} if the file is reviewed.
    */
   _hasOwnersReview(filename, subtree, isApproved) {
     return Object.entries(this.reviewers)
@@ -188,7 +188,7 @@ class OwnersCheck {
    *
    * @param {!string} filename file to check.
    * @param {!OwnersTree} subtree nearest ownership tree to file.
-   * @return {Owner[]} required owners that have not approved.
+   * @return {!Array<!Owner>} required owners that have not approved.
    */
   _missingRequiredOwners(filename, subtree) {
     return subtree
@@ -206,7 +206,7 @@ class OwnersCheck {
    *
    * @param {!string} filename file to check.
    * @param {!OwnersTree} subtree nearest ownership tree to file.
-   * @return {boolean} if the file has approval coverage.
+   * @return {!boolean} if the file has approval coverage.
    */
   _hasRequiredOwnersApproval(filename, subtree) {
     return this._missingRequiredOwners(filename, subtree).length === 0;
@@ -219,7 +219,7 @@ class OwnersCheck {
    *
    * @param {!string} filename file to check.
    * @param {!OwnersTree} subtree nearest ownership tree to file.
-   * @return {boolean} if the file is approved.
+   * @return {!boolean} if the file is approved.
    */
   _hasFullOwnersCoverage(filename, subtree) {
     return (
@@ -235,7 +235,7 @@ class OwnersCheck {
    *
    * @param {!string} filename file to check.
    * @param {!OwnersTree} subtree nearest ownership tree to file.
-   * @return {boolean} if the file is approved.
+   * @return {!boolean} if the file is approved.
    */
   _hasOwnersPendingReview(filename, subtree) {
     return this._hasOwnersReview(filename, subtree, false);
@@ -247,7 +247,7 @@ class OwnersCheck {
    *
    * Must be called after `init`.
    *
-   * @return {boolean} if the PR has reviewer approval.
+   * @return {!boolean} if the PR has reviewer approval.
    */
   _prHasReviewerSetApproval() {
     return Object.entries(this.reviewers)
@@ -261,7 +261,7 @@ class OwnersCheck {
    * Build the check-run comment describing current approval coverage.
    *
    * @param {!FileTreeMap} fileTreeMap map from filenames to ownership subtrees.
-   * @return {string} a list of files and which owners approved them, if any.
+   * @return {!string} a list of files and which owners approved them, if any.
    */
   buildCurrentCoverageText(fileTreeMap) {
     const allFilesText = Object.entries(fileTreeMap)
@@ -308,7 +308,7 @@ class OwnersCheck {
    * Build the check-run comment suggesting a reviewer set.
    *
    * @param {!ReviewerFiles} reviewSuggestions suggested reviewer set.
-   * @return {string} suggested reviewers and the files they could approve.
+   * @return {!string} suggested reviewers and the files they could approve.
    */
   buildReviewSuggestionsText(reviewSuggestions) {
     const suggestionsText = reviewSuggestions.map(
@@ -325,8 +325,8 @@ class OwnersCheck {
   /**
    * Build the check-run comment describing the need for a reviewer approval.
    *
-   * @param {Owner[]} reviewers list of reviewer owners.
-   * @return {string} explanation of reviewer set, if present.
+   * @param {!Array<!Owner>} reviewers list of reviewer owners.
+   * @return {!string} explanation of reviewer set, if present.
    */
   buildReviewerSetText(reviewers) {
     return (
