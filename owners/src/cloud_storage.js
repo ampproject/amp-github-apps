8ad3cead2ab7147086d1da21ea9e8719718a6489
Ryan Cebulko <ryan@cebulko.com>
2019-10-21 13:15:01 +0200

    Add interface for file operations on Cloud Storage

---
+++ owners/src/cloud_storage.js
@@ -0,0 +1,74 @@
+/**
+ * Copyright 2019 The AMP HTML Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const {Storage} = require('@google-cloud/storage');
+
+/**
+ * Google Cloud Storage interface for uploading and downloading files.
+ */
+class CloudStorage {
+  /**
+   * Constructor.
+   *
+   * @param {string} bucketName Cloud Storage bucket name.
+   */
+  constructor(bucketName) {
+    this.storage = new Storage();
+    this.bucketName = bucketName;
+  }
+
+  /**
+   * Get a reference to a file in storage.
+   *
+   * @param {string} filename file to access.
+   * @return {File} reference to a Cloud Storage file.
+   */
+  file(filename) {
+    return this.storage.bucket(this.bucketName).file(filename);
+  }
+
+  /**
+   * Write a file to storage.
+   *
+   * @param {string} filename file to write.
+   * @param {string} contents file contents.
+   */
+  async upload(filename, contents) {
+    await this.file(filename).save(contents, {resumable: false});
+  }
+
+  /**
+   * Read a file from storage.
+   *
+   * @param {string} filename file to download.
+   * @return {string} file contents.
+   */
+  async download(filename) {
+    const [contents] = await this.file(filename).download();
+    return contents.toString('utf8');
+  }
+
+  /**
+   * Delete a file in storage.
+   *
+   * @param {string} filename file to delete.
+   */
+  async delete(filename) {
+    await this.file(filename).delete();
+  }
+}
+
+module.exports = {CloudStorage};
