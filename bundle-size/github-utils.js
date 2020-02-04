e5dfde9d250ba2dba1f2185ac9ca38587092788f
Daniel Rozenberg <rodaniel@amp.dev>
2020-02-04 14:39:53 -0800

    Address feedback

--- bundle-size/github-utils.js
+++ bundle-size/github-utils.js
@@ -130,8 +130,8 @@ class GitHubUtils {
   /**
    * Get a list of all unique team members by team names.
    *
-   * @param {!Array<string>} teamNames list of GitHub team (full) slugs,
-   *   e.g., ["ampproject/wg-coffee", "ampproject/wg-tea"].
+   * @param {!Array<string>} teamNames list of GitHub team slugs. e.g.,
+   *   ["ampproject/wg-coffee", "ampproject/wg-tea"].
    * @return {!Array<string>} all members of the provided teams.
    */
   async getTeamMembers(teamNames) {
