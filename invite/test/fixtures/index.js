9652b973f2bed68557da48756dc5b295cc82eb42
Ryan Cebulko <ryan@cebulko.com>
2020-02-10 12:40:18 -0500

    Add getFixture test helper

---
+++ invite/test/fixtures/index.js
@@ -0,0 +1,29 @@
+/**
+ * Copyright 2020, the AMP HTML authors
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const fs = require('fs');
+const path = require('path');
+
+/**
+ * Get a JSON test fixture object.
+ *
+ * @param {!string} name name of the JSON fixture file (without .json).
+ * @return {!object} the named JSON test fixture file.
+ */
+exports.getFixture = name => {
+  return JSON.parse(
+    fs.readFileSync(path.join(__dirname, `${name}.json`))
+  );
+};
