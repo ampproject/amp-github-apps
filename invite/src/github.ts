d97bf2d4f318f8f51763b018ef8c86cd632d9c7f
Ryan Cebulko <ryan@cebulko.com>
2020-02-10 18:11:33 -0500

    Add method stubs and tests for GitHub API interface (#692)
    * Add sample responses for GitHub add-member calls
    
    * Add logger interface definition
    
    * Add GitHub method stubs
    
    * Add failing tests for GitHub API interface
    
    * Drop unused import
    
    * Fix Jest typescript config
    
    * Add .skip to github api tests

---
+++ invite/src/github.ts
@@ -0,0 +1,71 @@
+/**
+ * Copyright 2020 The AMP HTML Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {Octokit} from '@octokit/rest';
+
+import {ILogger} from './types';
+
+/**
+ * Interface for working with the GitHub API.
+ */
+export class GitHub {
+  private client: Octokit;
+  private org: string;
+  private logger: ILogger;
+
+  /**
+   * Constructor.
+   */
+  constructor(client: Octokit, org: string, logger: ILogger = console) {
+    Object.assign(this, {client, org, logger});
+  }
+
+  /**
+   * Checks whether a user is a member of the organization.
+   */
+  async userIsMember(username: string): Promise<boolean> {
+    // https://octokit.github.io/rest.js/#octokit-routes-orgs-check-membership
+    // octokit.orgs.checkMembership({org, username});
+    return false;
+  }
+
+  /**
+   * Attempts to invite a user to the organization.
+   */
+  async inviteUser(username: string): Promise<boolean> {
+    // https://octokit.github.io/rest.js/#octokit-routes-orgs-create-invitation
+    // octokit.orgs.addOrUpdateMembership({org, username})
+    return false;
+  }
+
+  /**
+   * Adds a comment to an issue.
+   */
+  async addComment(repo: string, issue_number: number, comment: string): Promise<void> {
+    // https://octokit.github.io/rest.js/#octokit-routes-issues-create-comment
+    // octokit.issues.createComment({owner, repo, issue_number, body})
+  }
+
+  /**
+   * Assigns an issue to a user.
+   */
+  async assignIssue(repo: string, issue_number: number, assignee: string): Promise<void> {
+    // https://octokit.github.io/rest.js/#octokit-routes-issues-add-assignee
+    // octokit.issues.addAssignees({owner, repo, issue_number, assignees})
+  }
+}
+
+module.exports = {GitHub};
---
+++ invite/test/github.test.ts
@@ -0,0 +1,137 @@
+/**
+ * Copyright 2020 The AMP HTML Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {createTokenAuth} from '@octokit/auth';
+import nock from 'nock';
+import {Octokit} from '@octokit/rest';
+
+import {getFixture} from './fixtures';
+import {GitHub} from '../src/github';
+
+describe.skip('GitHub interface', () => {
+  const githubClient: Octokit = new Octokit({
+    authStrategy: createTokenAuth,
+    auth: '_TOKEN_',
+  });
+  let github: GitHub;
+
+  beforeAll(() => {
+    nock.disableNetConnect();
+  });
+
+  afterAll(() => {
+    nock.enableNetConnect();
+  });
+
+  beforeEach(() => {
+    github = new GitHub(githubClient, 'test_org');
+  });
+
+  afterEach(() => {
+    // Fail the test if there were unused nocks.
+    if(!nock.isDone()) {
+      throw new Error('Not all nock interceptors were used!');
+      nock.cleanAll();
+    }
+  });
+
+  describe('userIsMember', () => {
+    it('GETs /orgs/:org/members/:username', async done => {
+      nock('https://api.github.com')
+        .get('/orgs/test_org/members/someone')
+        .reply(204);
+
+      await github.userIsMember('someone');
+      done();
+    });
+
+    it('returns true for 204: No Content', async done => {
+      nock('https://api.github.com')
+        .get('/orgs/test_org/members/someone')
+        .reply(204);
+
+      expect(await github.userIsMember('someone')).toBe(true);
+      done();
+    });
+
+    it('returns false for 404: Not Found', async done => {
+      nock('https://api.github.com')
+        .get('/orgs/test_org/members/someone')
+        .reply(404);
+
+      expect(await github.userIsMember('someone')).toBe(false);
+      done();
+    });
+  });
+
+  describe('inviteUser', () => {
+    it('PUTs to /orgs/:org/memberships/:username', async done => {
+      nock('https://api.github.com')
+        .put('/orgs/test_org/memberships/someone')
+        .reply(200, getFixture('add_member.exists'))
+
+        await github.inviteUser('someone');
+        done();
+    });
+
+    it('returns true when the user is invited', async done => {
+      nock('https://api.github.com')
+        .put('/orgs/test_org/memberships/someone')
+        .reply(200, getFixture('add_member.invited'))
+
+        expect(await github.inviteUser('someone')).toBe(true);
+        done();
+    });
+
+    it('returns false when the user is already a member', async done => {
+      nock('https://api.github.com')
+        .put('/orgs/test_org/memberships/someone')
+        .reply(200, getFixture('add_member.exists'))
+
+        expect(await github.inviteUser('someone')).toBe(false);
+        done();
+    });
+  });
+
+  describe('addComment', () => {
+    it('POSTs comment to /repos/:owner/:repo/:issue_number/comment', async done => {
+      nock('https://api.github.com')
+        .post('/repos/test_org/test_repo/1337/comment', body => {
+          expect(body).toEqual({ body: 'Test comment' });
+          return true;
+        })
+        .reply(200);
+
+      await github.addComment('test_repo', 1337, 'Test comment');
+      done();
+    });
+  });
+
+  describe('assignIssue', () => {
+    it('POSTs assignee to /repos/:owner/:repo/issues/:issue_number/assignees',
+        async done => {
+      nock('https://api.github.com')
+        .post('/repos/test_org/test_repo/issues/1337/assignees', body => {
+          expect(body).toEqual({ assignees: ['someone'] });
+          return true;
+        })
+        .reply(200);
+
+      await github.assignIssue('test_repo', 1337, 'someone');
+      done();
+    });
+  });
+});
