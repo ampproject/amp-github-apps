c45e58e0f79d2f48ac222138f9b16f7b28441233
Esther Kim <44627152+estherkim@users.noreply.github.com>
2020-03-05 15:18:44 -0500

    [release calendar] Add React and Webpack for client side (#736)

---
+++ release-calendar/webpack.client.config.js
@@ -0,0 +1,63 @@
+/**
+ * Copyright 2020 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: "production",
+  devtool: "source-map",
+  devServer: {
+    port: 9000
+  },
+  entry: './src/client/index.tsx',
+  resolve: {
+    extensions: [".wasm", ".mjs", ".js", ".jsx", ".ts", ".tsx", ".json"],
+  },
+  output: {
+    filename: 'bundle-client.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader"
+          }
+        ]
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/index.html",
+      "filename": "./index.html"
+    })
+  ],
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM",
+  }
+}
