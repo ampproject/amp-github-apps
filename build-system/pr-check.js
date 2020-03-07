b00fea5cfbe8a7ec17969953ca42d6cabb133891
Alan Orozco <alanorozco@users.noreply.github.com>
2020-03-07 13:13:36 -0800

    Update pr-check.js

--- build-system/pr-check.js
+++ build-system/pr-check.js
@@ -67,6 +67,9 @@ function main() {
   if (buildTargets.has('BUNDLE_SIZE')) {
     runAppTests('bundle-size');
   }
+  if (buildTargets.has('CHECKLIST')) {
+    runAppTests('checklist');
+  }
   if (buildTargets.has('INVITE')) {
     runAppTests('invite');
   }
