ea9aa082b70195042ba3d6b57a6dcda5b9a1f581
Anna Watson <watsonanna@google.com>
2020-03-10 18:56:06 -0400

    un test test

--- release-calendar/src/client/components/App.tsx
+++ release-calendar/src/client/components/App.tsx
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-//////////
 import * as React from 'react';
 import {Header} from './Header';
 import {Channel, CurrentRelease, RTVRowObject} from '../../types';
