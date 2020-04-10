0929bcb66ad4ea6d2a4b19142ba41935d7d4771d
Anna Watson <watsonanna@google.com>
2020-04-09 19:21:52 -0700

    clean up by removing if statement

--- release-calendar/test/repository-service.ts
+++ release-calendar/test/repository-service.ts
@@ -41,9 +41,7 @@ export class RepositoryService {
 
   async createRelease(release: Release, date?: Date): Promise<Release> {
     const entity = await this.releaseRepository.save(release);
-    const promotion = date
-      ? new Promotion(entity, Channel.CREATED, Channel.NIGHTLY, date)
-      : new Promotion(entity, Channel.CREATED, Channel.NIGHTLY);
+    const promotion = new Promotion(entity, Channel.CREATED, Channel.NIGHTLY, date);
     await this.savePromotion(promotion);
     return this.getRelease(entity.name);
   }
