c0a2f87b59f91f3e5d2829b270d4e90ff65fde77
Ryan Cebulko <ryan@cebulko.com>
2020-04-16 16:41:47 -0400

    [error-issue] Creates error-issue bot to glue blame finding and issue creation (#781)
    * Add stubs for main classes
    
    * Add http-status-codes as dependency
    
    * Add method to generate issue
    
    * Add method to create the GitHub issue
    
    * Update request handler to use bot
    
    * Lint fixes
    
    * Fail tests with unexpected requests
    
    * Fix default repo
    
    * Lint fixes

--- error-issue/index.ts
+++ error-issue/index.ts
@@ -15,7 +15,44 @@
  */
 
 import express from 'express';
+import statusCodes from 'http-status-codes';
+import {ErrorIssueBot} from './src/bot';
+import {ErrorReport} from './src/types';
 
-module.exports.app = (req: express.Request, res: express.Response) => {
-  res.sendStatus(200);
+const GITHUB_REPO = process.env.GITHUB_REPO || 'ampproject/amphtml';
+const [GITHUB_REPO_OWNER, GITHUB_REPO_NAME] = GITHUB_REPO.split('/');
+const GITHUB_ACCESS_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
+
+const bot = new ErrorIssueBot(
+  GITHUB_ACCESS_TOKEN,
+  GITHUB_REPO_OWNER,
+  GITHUB_REPO_NAME
+);
+
+module.exports.app = async (req: express.Request, res: express.Response) => {
+  const errorReport = req.method === 'POST' ? req.body : req.query;
+  const {errorId, firstSeen, dailyOccurrences, stacktrace} = errorReport;
+
+  if (!(errorId && firstSeen && dailyOccurrences && stacktrace)) {
+    res.status(statusCodes.BAD_REQUEST);
+    return res.send('Missing error report params');
+  }
+
+  console.debug(`Processing http://go/ampe/${errorId}`);
+  const parsedReport: ErrorReport = {
+    errorId,
+    firstSeen: new Date(firstSeen),
+    dailyOccurrences: parseInt(dailyOccurrences, 10),
+    stacktrace,
+  };
+
+  try {
+    const issueUrl = await bot.report(parsedReport);
+    res.redirect(302, issueUrl);
+  } catch (errResp) {
+    console.warn(errResp);
+    res.status(errResp.status || 500);
+    res.set('Content-Type', 'application/json');
+    res.send(JSON.stringify(errResp, null, 2));
+  }
 };
