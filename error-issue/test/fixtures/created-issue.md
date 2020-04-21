f9dddd8fb01361c4bf2e73d5445ef91ec94c64eb
Ryan Cebulko <ryan@cebulko.com>
2020-04-21 14:50:21 -0400

    Use fixture file for created issue markdown

---
+++ error-issue/test/fixtures/created-issue.md
@@ -0,0 +1,18 @@
+Details
+---
+**Error report:** [link](go/ampe/CL6chqbN2-bzBA)
+**First seen:** Feb 25, 2020
+**Frequency:** ~ 54,647/day
+
+Stacktrace
+---
+```
+Error: null is not an object (evaluating 'b.acceleration.x')
+    at x (https://raw.githubusercontent.com/ampproject/amphtml/2004030010070/extensions/amp-delight-player/0.1/amp-delight-player.js:421:13)
+    at event (https://raw.githubusercontent.com/ampproject/amphtml/2004030010070/src/event-helper-listen.js:58:27)
+```
+
+Notes
+---
+`@xymw` modified `extensions/amp-delight-player/0.1/amp-delight-player.js:396-439` in #17939 (Nov 12, 2018)
+`@rsimha` modified `src/event-helper-listen.js:57-59` in #12450 (Dec 13, 2017)
--- error-issue/test/issue_builder.test.ts
+++ error-issue/test/issue_builder.test.ts
@@ -16,25 +16,7 @@
 
 import {IssueBuilder} from '../src/issue_builder';
 import {ErrorReport} from '../src/types';
-
-const FULL_ISSUE_BODY = `Details
----
-**Error report:** [link](go/ampe/CL6chqbN2-bzBA)
-**First seen:** Feb 25, 2020
-**Frequency:** ~ 54,647/day
-
-Stacktrace
----
-\`\`\`
-Error: null is not an object (evaluating 'b.acceleration.x')
-    at x (https://raw.githubusercontent.com/ampproject/amphtml/2004030010070/extensions/amp-delight-player/0.1/amp-delight-player.js:421:13)
-    at event (https://raw.githubusercontent.com/ampproject/amphtml/2004030010070/src/event-helper-listen.js:58:27)
-\`\`\`
-
-Notes
----
-\`@xymw\` modified \`extensions/amp-delight-player/0.1/amp-delight-player.js:396-439\` in #17939 (Nov 12, 2018)
-\`@rsimha\` modified \`src/event-helper-listen.js:57-59\` in #12450 (Dec 13, 2017)`;
+import {getFixtureFile} from './fixtures';
 
 describe('IssueBuilder', () => {
   let builder: IssueBuilder;
@@ -139,7 +121,7 @@ describe('IssueBuilder', () => {
 
   describe('body', () => {
     it('generates the full issue body markdown', () => {
-      expect(builder.body).toEqual(FULL_ISSUE_BODY);
+      expect(builder.body).toEqual(getFixtureFile('created-issue.md').trim());
     });
   });
 });
