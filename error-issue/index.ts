c3165d3539db664eb02eeb5640d5201d3826ec78
Ryan Cebulko <ryan@cebulko.com>
2020-04-21 13:50:45 -0400

    WIP: attempt at handler

--- error-issue/index.ts
+++ error-issue/index.ts
@@ -17,11 +17,14 @@
 import express from 'express';
 import statusCodes from 'http-status-codes';
 import {ErrorIssueBot} from './src/bot';
+import {StackdriverApi} from './src/stackdriver_api';
+import {ErrorMonitor} from './src/error_monitor';
 import {ErrorReport} from './src/types';
 
 const GITHUB_REPO = process.env.GITHUB_REPO || 'ampproject/amphtml';
 const [GITHUB_REPO_OWNER, GITHUB_REPO_NAME] = GITHUB_REPO.split('/');
 const GITHUB_ACCESS_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
+const PROJECT_ID = process.env.PROJECT_ID || 'amp-error-reporting';
 
 const bot = new ErrorIssueBot(
   GITHUB_ACCESS_TOKEN,
@@ -29,7 +32,17 @@ const bot = new ErrorIssueBot(
   GITHUB_REPO_NAME
 );
 
-module.exports.app = async (req: express.Request, res: express.Response) => {
+const client = new StackdriverApi(
+  process.env.OAUTH_TOKEN,
+  PROJECT_ID
+);
+const monitor = new ErrorMonitor(client);
+
+/** Endpoint to create a GitHub issue for an error report. */
+module.exports.errorIssue = async (
+  req: express.Request,
+  res: express.Response,
+) => {
   const errorReport = req.method === 'POST' ? req.body : req.query;
   const {errorId, firstSeen, dailyOccurrences, stacktrace} = errorReport;
 
@@ -54,5 +67,18 @@ module.exports.app = async (req: express.Request, res: express.Response) => {
     res.status(errResp.status || 500);
     res.set('Content-Type', 'application/json');
     res.send(JSON.stringify(errResp, null, 2));
+  }
+};
+
+/** Endpoint to trigger a scan for new frequent errors. */
+module.exports.errorMonitor = async (
+  req: express.Request,
+  res: express.Response,
+) => {
+  try {
+    res.json({issues: await monitor.monitorAndReport});
+  } catch (error) {
+    res.status(500);
+    res.json(error);
   }
 };
