8cf43ca6c8380b0f76f6c92a6048833eecdce18d
Ryan Cebulko <ryan@cebulko.com>
2020-04-21 13:40:29 -0400

    Add module to monitor and report new errors

---
+++ error-issue/src/error_monitor.ts
@@ -0,0 +1,105 @@
+/**
+ * Copyright 2020 The AMP HTML Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {StackdriverApi} from './stackdriver_api';
+import {ErrorReport, Stackdriver} from './types';
+
+import fetch from 'node-fetch';
+import statusCodes from 'http-status-codes';
+
+// const ERROR_ISSUE_REPORT_ENDPOINT = 'http://localhost:8080';
+const ERROR_ISSUE_REPORT_ENDPOINT =
+  'https://us-central1-amp-error-issue-bot.cloudfunctions.net/error-issue';
+
+export class ErrorMonitor {
+  constructor(
+    private client: StackdriverApi,
+    private minFrequency: number = 5000,
+    private pageLimit: number = 25
+  ) {}
+
+  /** Tests if an error group already has an associated issue. */
+  private hasTrackingIssue(group: Stackdriver.ErrorGroup) {
+    return !!group.trackingIssues;
+  }
+
+  /** Tests if an error group is occurring frequently enough to report. */
+  private hasHighFrequency(groupStats: Stackdriver.ErrorGroupStats) {
+    const timedCount = groupStats.timedCounts[0];
+    return timedCount && timedCount.count >= this.minFrequency;
+  }
+
+  /** Finds frequent errors to create tracking issues for. */
+  async newErrorsToReport(): Promise<Array<Stackdriver.ErrorGroupStats>> {
+    return (await this.client.listGroups(this.pageLimit))
+      .filter(({group}) => !this.hasTrackingIssue(group))
+      .filter(groupStats => this.hasHighFrequency(groupStats));
+  }
+
+  /** Reports an error to the bot endpoint, returning the created issue URL. */
+  async reportError({
+    group,
+    firstSeenTime,
+    timedCounts,
+    representative,
+  }: Stackdriver.ErrorGroupStats): Promise<string> {
+    console.info(`Reporting error group ${group.groupId} to error issue bot`);
+    const errorReport: ErrorReport = {
+      errorId: group.groupId,
+      firstSeen: firstSeenTime,
+      dailyOccurrences: timedCounts[0].count,
+      stacktrace: representative.message,
+    };
+    const {status, statusText, headers} = await fetch(
+      ERROR_ISSUE_REPORT_ENDPOINT,
+      {
+        method: 'POST',
+        redirect: 'manual',
+        body: JSON.stringify(errorReport),
+        headers: {'Content-Type': 'application/json'},
+      }
+    );
+
+    if (status !== statusCodes.MOVED_TEMPORARILY) {
+      throw new Error(
+        `HTTP ${status} (${statusText}): ` +
+          `Failed to file GitHub issue for "${group.groupId}`
+      );
+    }
+
+    const issueUrl = headers.get('Location');
+    console.info(`Successfully created error report issue: ${issueUrl}`);
+    return issueUrl;
+  }
+
+  /** Identifies new, frequent errors and reports GitHub issues. */
+  async monitorAndReport() {
+    const errors = await this.newErrorsToReport();
+    const urls: Array<string> = [];
+
+    for (const error of errors) {
+      try {
+        const url = await this.reportError(error);
+        urls.push(url);
+        await this.client.setGroupIssue(error.group.groupId, url);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    return urls;
+  }
+}
---
+++ error-issue/test/error_monitor.test.ts
@@ -0,0 +1,200 @@
+/**
+ * Copyright 2020 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {ErrorMonitor} from '../src/error_monitor';
+import {StackdriverApi} from '../src/stackdriver_api';
+import {Stackdriver} from '../src/types';
+
+import nock from 'nock';
+import {mocked} from 'ts-jest/utils';
+
+describe('ErrorMonitor', () => {
+  let monitor: ErrorMonitor;
+  const stackdriver = ({
+    listGroups: jest.fn(),
+    setGroupIssue: jest.fn(),
+  } as unknown) as StackdriverApi;
+
+  const infrequentGroup: Stackdriver.ErrorGroupStats = {
+    group: {
+      name: 'Error: Infrequent error',
+      groupId: 'abc123',
+    },
+    count: 2000,
+    timedCounts: [
+      {
+        count: 4,
+        startTime: new Date('Feb 25, 2020'),
+        endTime: new Date('Feb 26, 2020'),
+      },
+    ],
+    firstSeenTime: new Date('Feb 20, 2020'),
+    representative: {message: 'Error: Infrequent error'},
+  };
+  const acknowledgedGroup: Stackdriver.ErrorGroupStats = {
+    group: {
+      name: 'Error: Acknowledged error',
+      groupId: 'def456',
+      trackingIssues: [{url: 'https://github.com/blah/blah'}],
+    },
+    count: 20000,
+    timedCounts: [
+      {
+        count: 6000,
+        startTime: new Date('Feb 25, 2020'),
+        endTime: new Date('Feb 26, 2020'),
+      },
+    ],
+    firstSeenTime: new Date('Feb 20, 2020'),
+    representative: {message: 'Error: Acknowledged error'},
+  };
+  const newGroup: Stackdriver.ErrorGroupStats = {
+    group: {
+      name: 'Error: New error',
+      groupId: 'ghi789',
+    },
+    count: 20000,
+    timedCounts: [
+      {
+        count: 6000,
+        startTime: new Date('Feb 25, 2020'),
+        endTime: new Date('Feb 26, 2020'),
+      },
+    ],
+    firstSeenTime: new Date('Feb 20, 2020'),
+    representative: {message: 'Error: New error'},
+  };
+  const errorGroups = [infrequentGroup, acknowledgedGroup, newGroup];
+
+  beforeAll(() => {
+    nock.disableNetConnect();
+  });
+
+  afterAll(() => {
+    nock.enableNetConnect();
+  });
+
+  beforeEach(() => {
+    monitor = new ErrorMonitor(stackdriver);
+    mocked(stackdriver.listGroups).mockResolvedValue(errorGroups);
+    nock.cleanAll();
+  });
+
+  afterEach(() => {
+    // Fail the test if there were unused nocks.
+    if (!nock.isDone()) {
+      throw new Error('Not all nock interceptors were used!');
+    }
+    nock.cleanAll();
+  });
+
+  describe('newErrorsToReport', () => {
+    it('ignores infrequent errors', async () => {
+      await expect(monitor.newErrorsToReport()).resolves.not.toContain(
+        infrequentGroup
+      );
+    });
+
+    it('ignores errors with an existing tracking issue', async () => {
+      await expect(monitor.newErrorsToReport()).resolves.not.toContain(
+        acknowledgedGroup
+      );
+    });
+
+    it('includes new frequent errors', async () => {
+      await expect(monitor.newErrorsToReport()).resolves.toContain(newGroup);
+    });
+  });
+
+  describe('reportError', () => {
+    it('reports the error to the error-issue endpoint', async () => {
+      nock('https://us-central1-amp-error-issue-bot.cloudfunctions.net')
+        .post('/error-issue', body => {
+          expect(body).toMatchObject({
+            errorId: 'ghi789',
+            firstSeen: expect.stringMatching(/^2020-02-20T/),
+            dailyOccurrences: 6000,
+            stacktrace: 'Error: New error',
+          });
+          return true;
+        })
+        .reply(302);
+
+      await monitor.reportError(newGroup);
+    });
+
+    it('returns the URL of the created issue', async () => {
+      nock('https://us-central1-amp-error-issue-bot.cloudfunctions.net')
+        .post('/error-issue')
+        .reply(302, null, { Location: 'http://github.com.com/blah/blah' });
+
+      await expect(monitor.reportError(newGroup))
+        .resolves
+        .toEqual('http://github.com.com/blah/blah');
+    });
+
+    it('throws an error when error creation fails', async () => {
+      nock('https://us-central1-amp-error-issue-bot.cloudfunctions.net')
+        .post('/error-issue')
+        .reply(400);
+
+      await expect(monitor.reportError(newGroup))
+        .rejects
+        .toThrow('HTTP 400 (Bad Request)');
+    });
+  });
+
+  describe('monitorAndReport', () => {
+    beforeEach(() => {
+      monitor.reportError = jest.fn().mockReturnValue(
+        'https://github.com/blah/blah'
+      );
+    });
+
+    it('reports new errors', async () => {
+      await monitor.monitorAndReport();
+
+      expect(monitor.reportError).toHaveBeenCalledTimes(1);
+      expect(monitor.reportError).toHaveBeenCalledWith(newGroup);
+    });
+
+    it('returns created URLs', async () => {
+      await expect(monitor.monitorAndReport()).resolves.toEqual([
+        'https://github.com/blah/blah'
+      ]);
+    });
+
+    it('sets tracking issues for new errors', async () => {
+      await monitor.monitorAndReport()
+
+      expect(stackdriver.setGroupIssue).toHaveBeenCalledWith(
+        'ghi789',
+        'https://github.com/blah/blah',
+      );
+    });
+
+    it('handles failed attempts to report errors', async () => {
+      jest.spyOn(console, 'error').mockImplementation(() => {});
+      monitor.reportError = jest.fn().mockImplementation(async () => {
+        throw new Error('Oops!');
+      });
+
+      await expect(monitor.monitorAndReport()).resolves.toEqual([]);
+      expect(console.error).toHaveBeenCalled();
+      mocked(console.error).mockRestore();
+    });
+  });
+});
