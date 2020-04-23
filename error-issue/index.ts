f6ce0e6f312cf0e1be4628c3285299248c3802a8
Ryan Cebulko <ryan@cebulko.com>
2020-04-23 11:59:22 -0400

    Add module to monitor errors in GCP and identify new untracked ones (#790)
    * Add node-fetch and google auth as dependencies
    
    * Add stackdriver API interface
    
    * Add module to monitor and report new errors
    
    * Use substitution variables in cloud_build.yaml
    
    * Add endpoint to scan for new error reports
    
    * Add diagnostic endpoint to view error issue candidates
    
    * Make error issue endpoint URL env variable
    
    * Reorder methods + JSDoc
    
    * Code review tweak
    
    * Lint fixes
    
    * Remove unused TrackingIssue interface
    
    * Reorder interfaces to group exports
    
    * Use statusCodes for 500 error reponses
    
    * Standardize exports in index
    
    * Move Google Auth scope to const

--- error-issue/index.ts
+++ error-issue/index.ts
@@ -19,11 +19,14 @@ require('dotenv').config();
 import express from 'express';
 import statusCodes from 'http-status-codes';
 import {ErrorIssueBot} from './src/bot';
+import {StackdriverApi} from './src/stackdriver_api';
+import {ErrorMonitor, ERROR_ISSUE_ENDPOINT} from './src/error_monitor';
 import {ErrorReport} from './src/types';
 
 const GITHUB_REPO = process.env.GITHUB_REPO || 'ampproject/amphtml';
 const [GITHUB_REPO_OWNER, GITHUB_REPO_NAME] = GITHUB_REPO.split('/');
 const GITHUB_ACCESS_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
+const PROJECT_ID = process.env.PROJECT_ID || 'amp-error-reporting';
 
 const bot = new ErrorIssueBot(
   GITHUB_ACCESS_TOKEN,
@@ -31,7 +34,11 @@ const bot = new ErrorIssueBot(
   GITHUB_REPO_NAME
 );
 
-module.exports.app = async (req: express.Request, res: express.Response) => {
+const stackdriver = new StackdriverApi(PROJECT_ID);
+const monitor = new ErrorMonitor(stackdriver);
+
+/** Endpoint to create a GitHub issue for an error report. */
+export async function errorIssue(req: express.Request, res: express.Response) {
   const errorReport = req.method === 'POST' ? req.body : req.query;
   const {errorId, firstSeen, dailyOccurrences, stacktrace} = errorReport;
 
@@ -50,11 +57,48 @@ module.exports.app = async (req: express.Request, res: express.Response) => {
 
   try {
     const issueUrl = await bot.report(parsedReport);
-    res.redirect(302, issueUrl);
+    res.redirect(statusCodes.MOVED_TEMPORARILY, issueUrl);
   } catch (errResp) {
     console.warn(errResp);
-    res.status(errResp.status || 500);
+    res.status(errResp.status || statusCodes.INTERNAL_SERVER_ERROR);
     res.set('Content-Type', 'application/json');
     res.send(JSON.stringify(errResp, null, 2));
   }
-};
+}
+
+/** Endpoint to trigger a scan for new frequent errors. */
+export async function errorMonitor(
+  req: express.Request,
+  res: express.Response
+) {
+  try {
+    res.json({issueUrls: await monitor.monitorAndReport()});
+  } catch (error) {
+    res.status(statusCodes.INTERNAL_SERVER_ERROR);
+    res.json({error: error.toString()});
+  }
+}
+
+function createErrorReportUrl(report: ErrorReport) {
+  const params = Object.entries(report)
+    .map(([key, val]) => `${key}=${encodeURIComponent(val)}`)
+    .join('&');
+
+  return `${ERROR_ISSUE_ENDPOINT}?${params}`;
+}
+
+/** Diagnostic endpoint to list new untracked errors. */
+export async function errorList(req: express.Request, res: express.Response) {
+  try {
+    const reports = await monitor.newErrorsToReport();
+    res.json({
+      errorReports: reports.map(report => ({
+        createUrl: createErrorReportUrl(report),
+        ...report,
+      })),
+    });
+  } catch (error) {
+    res.status(statusCodes.INTERNAL_SERVER_ERROR);
+    res.json({error: error.toString()});
+  }
+}
---
+++ error-issue/src/error_monitor.ts
@@ -0,0 +1,109 @@
+/**
+ * Copyright 2020 The AMP HTML Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {StackdriverApi} from './stackdriver_api';
+import {ErrorReport, Stackdriver} from './types';
+
+import fetch from 'node-fetch';
+import statusCodes from 'http-status-codes';
+
+export const ERROR_ISSUE_ENDPOINT =
+  process.env.ERROR_ISSUE_ENDPOINT ||
+  'https://us-central1-amp-error-issue-bot.cloudfunctions.net/error-issue';
+
+export class ErrorMonitor {
+  constructor(
+    private client: StackdriverApi,
+    private minFrequency: number = 5000,
+    private pageLimit: number = 25
+  ) {}
+
+  /** Tests if an error group already has an associated issue. */
+  private hasTrackingIssue(group: Stackdriver.ErrorGroup) {
+    return !!group.trackingIssues;
+  }
+
+  /** Tests if an error group is occurring frequently enough to report. */
+  private hasHighFrequency(groupStats: Stackdriver.ErrorGroupStats) {
+    const timedCount = groupStats.timedCounts[0];
+    return timedCount && timedCount.count >= this.minFrequency;
+  }
+
+  /** Converts error group stats to an error report to be filed. */
+  private reportFromGroupStats({
+    group,
+    firstSeenTime,
+    timedCounts,
+    representative,
+  }: Stackdriver.ErrorGroupStats): ErrorReport {
+    return {
+      errorId: group.groupId,
+      firstSeen: firstSeenTime,
+      dailyOccurrences: timedCounts[0].count,
+      stacktrace: representative.message,
+    };
+  }
+
+  /** Finds frequent errors to create tracking issues for. */
+  async newErrorsToReport(): Promise<Array<ErrorReport>> {
+    return (await this.client.listGroups(this.pageLimit))
+      .filter(({group}) => !this.hasTrackingIssue(group))
+      .filter(groupStats => this.hasHighFrequency(groupStats))
+      .map(groupStats => this.reportFromGroupStats(groupStats));
+  }
+
+  /** Reports an error to the bot endpoint, returning the created issue URL. */
+  async reportError(errorReport: ErrorReport): Promise<string> {
+    const {errorId} = errorReport;
+    console.info(`Reporting error group ${errorId} to error issue bot`);
+    const {status, statusText, headers} = await fetch(ERROR_ISSUE_ENDPOINT, {
+      method: 'POST',
+      redirect: 'manual',
+      body: JSON.stringify(errorReport),
+      headers: {'Content-Type': 'application/json'},
+    });
+
+    if (status !== statusCodes.MOVED_TEMPORARILY) {
+      throw new Error(
+        `HTTP ${status} (${statusText}): ` +
+          `Failed to file GitHub issue for "${errorId}`
+      );
+    }
+
+    const issueUrl = headers.get('Location');
+    console.info(`Successfully created error report issue: ${issueUrl}`);
+    return issueUrl;
+  }
+
+  /** Identifies new, frequent errors and reports GitHub issues. */
+  async monitorAndReport() {
+    const errors = await this.newErrorsToReport();
+    console.info(`Found ${errors.length} new error groups to report`);
+    const urls: Array<string> = [];
+
+    for (const error of errors) {
+      try {
+        const url = await this.reportError(error);
+        urls.push(url);
+        await this.client.setGroupIssue(error.errorId, url);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    return urls;
+  }
+}
