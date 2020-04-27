868be708fd3b64d8e57c32fd70e28ae3ed9b258c
Ryan Cebulko <ryan@cebulko.com>
2020-04-27 18:19:43 -0400

    Fix tests

--- error-issue/test/blame_finder.test.ts
+++ error-issue/test/blame_finder.test.ts
@@ -228,10 +228,18 @@ describe('BlameFinder', () => {
         path: 'extensions/amp-base-carousel/0.1/dimensions.js',
         startingLine: 58,
         endingLine: 58,
-        author: 'rsimha',
+        author: '@rsimha',
         committedDate: new Date('2019-05-16T14:59:15Z'),
         changedFiles: 1623,
         prNumber: 21212
+      }, {
+        path: 'extensions/amp-base-carousel/0.1/dimensions.js',
+        startingLine: 67,
+        endingLine: 86,
+        author: 'Sepand Parhami',
+        committedDate: new Date('2019-01-17T22:56:30Z'),
+        changedFiles: 2,
+        prNumber: 20389
       }]);
     });
   });
