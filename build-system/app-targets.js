d31d0ed892ee0063c7c02c321252dbea015afa11
Ryan Cebulko <ryan@cebulko.com>
2020-05-01 16:56:49 -0400

    Simplify build targets for apps

---
+++ build-system/app-targets.js
@@ -0,0 +1,11 @@
+module.exports = {
+  BUNDLE_SIZE: 'bundle-size',
+  CHECKLIST: 'checklist',
+  ERROR_ISSUE: 'error-issue',
+  INVITE: 'invite',
+  ONDUTY: 'onduty',
+  OWNERS: 'owners',
+  PR_DEPLOY: 'pr-deploy',
+  RELEASE_CALENDAR: 'release-calendar',
+  TEST_STATUS: 'test-status',
+};
