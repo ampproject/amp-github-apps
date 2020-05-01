01087c3fe6b46ad7a9c9a5a109d5d90a59ee166f
esth <esth@google.com>
2020-05-01 14:05:03 -0400

    save

--- release-calendar/server.js
+++ release-calendar/server.js
@@ -25,12 +25,6 @@ const DIST_DIR = path.join(__dirname, 'dist');
 
 app.use(express.static(DIST_DIR));
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
-});
-
-//  apply to all requests
 app.use(
   rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
