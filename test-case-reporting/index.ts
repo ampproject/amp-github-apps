fe4217261a9214240544ab9ad02e88015b2c1a43
rafer45 <ataque992@hotmail.com>
2020-06-09 16:45:57 -0400

    Set correct default port

--- test-case-reporting/index.ts
+++ test-case-reporting/index.ts
@@ -16,7 +16,7 @@
 
 import express from 'express';
 const app = express();
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
