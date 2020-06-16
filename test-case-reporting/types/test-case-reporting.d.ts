62ab8b844353f3f3c8c4b7081006590efe252c69
Rafer45 <ataque992@hotmail.com>
2020-06-16 17:45:39 -0400

    [test-case-reporting] Add types for Travis test reports (#906)
    * Add types for Travis test reports
    
    * Improve type names
    
    * Rename types & make them fit json exactly
    
    * Rename TestResult
    
    Co-authored-by: Ryan Cebulko <ryan@cebulko.com>
    
    * Rename TestResultReport Browsers
    
    Co-authored-by: Ryan Cebulko <ryan@cebulko.com>
    
    * Add namespace & remove unused type
    
    Co-authored-by: Ryan Cebulko <ryan@cebulko.com>

--- test-case-reporting/types/test-case-reporting.d.ts
+++ test-case-reporting/types/test-case-reporting.d.ts
@@ -60,4 +60,23 @@ declare module 'test-case-reporting' {
     timestamp: Date,
     durationMs: number,
   }
+
+  namespace KarmaReporter {
+    export interface TestResultReport {
+      browsers: BrowserResultSet,
+    }
+
+    export interface BrowserResultSet {
+      results: Array<TestResult>
+    }
+
+    export interface TestResult {
+      description: string,
+      suite: Array<string>,
+      success: boolean,
+      skipped: boolean,
+      pending: boolean,
+      time: number, // in milliseconds
+    }
+  }
 }
