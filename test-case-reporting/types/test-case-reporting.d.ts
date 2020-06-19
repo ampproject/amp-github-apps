e81f3fc0a2f4661ef156b55ea596af5902c45e0e
Rafer45 <ataque992@hotmail.com>
2020-06-19 13:01:36 -0400

    [test-case-reporting] Create types for interfacing with database (#907)
    * Create types for interfacing with database
    
    * Rename & clarify testSuiteType
    
    * Add types to typescript settings
    
    * Change commas in interfaces to semicolons
    
    * Tweak lint rules in DB type namespace
    
    * Empty commit to trigger Travis
    
    * Fix test_suite_type type
    
    * Remove unused  field

--- test-case-reporting/types/test-case-reporting.d.ts
+++ test-case-reporting/types/test-case-reporting.d.ts
@@ -18,65 +18,105 @@ declare module 'test-case-reporting' {
   /**
    * Travis job types for which test results may be reported.
    */
-  export type JobType = 'unit' | 'integration';
+  export type TestSuiteType = 'unit' | 'integration';
 
-  export type TestStatus = 'pass' | 'fail' | 'skip' | 'error';
+  export type TestStatus = 'PASS' | 'FAIL' | 'SKIP' | 'ERROR';
 
   /** A build on Travis. */
   export interface Build {
-    username: string,
-    commitHash: string,
-    prNumber: number,
-    startedAt: Date,
+    username: string;
+    commitHash: string;
+    prNumber: number;
+    startedAt: Date;
 
     // The list of jobs we know are contained in the build.
     // When we create a build, we fill this with its jobs, but when
     // we get a build from a server, the jobs list may not be populated
     // if the jobs are not needed.
-    jobs: Array<Job>,
+    jobs: Array<Job>;
   }
 
   /** A job within a Travis build. */
   export interface Job {
-    jobNumber: string,
-    type: JobType,
-    startedAt: Date,
+    jobNumber: string;
+    testSuiteType: TestSuiteType;
+    startedAt: Date;
 
     // This list is treated similarly to the `jobs` array of the `Build` type.
     // Read the comment on `jobs` for info.
-    testRuns: Array<TestRun>,
+    testRuns: Array<TestRun>;
   }
 
   /** A single kind of test case, one `it` or `test` block. */
   export interface TestCase {
-    name: string,
-    createdAt: Date,
+    name: string;
+    createdAt: Date;
   }
 
   /** An instance of a test being run, with results. */
   export interface TestRun {
-    testCase: TestCase,
-    status: TestStatus,
-    timestamp: Date,
-    durationMs: number,
+    testCase: TestCase;
+    status: TestStatus;
+    timestamp: Date;
+    durationMs: number;
+  }
+
+  // Types in the DB namespace interface with the database which
+  // uses snake_case instead of camelCase.
+  /* eslint @typescript-eslint/camelcase: "off" */
+  namespace DB {
+    export interface Build {
+      // `id` is nullable because is not set when uploading, it is only set
+      // when we get the build from the database.
+      // It is not nullable in the database.
+      id?: number;
+      commit_hash: string;
+      pull_request_number: number;
+      started_at: number;
+    }
+
+    export interface Job {
+      // See comment under `DB.Build.id`
+      id?: number;
+      buld_id: number;
+      job_number: string;
+      test_suite_type: TestSuiteType;
+      started_at: number;
+    }
+
+    export interface TestCase {
+      // See comment under `DB.Build.id`
+      id?: number;
+      name: string;
+      created_at: number;
+    }
+
+    export interface TestRun {
+      // See comment under `DB.Build.id`
+      id?: number;
+      job_id: number;
+      test_case_id: number;
+      status: TestStatus;
+      timestamp: number;
+      duration_ms: number;
+    }
   }
 
   namespace KarmaReporter {
     export interface TestResultReport {
-      browsers: BrowserResultSet,
+      browsers: BrowserResultSet;
     }
 
     export interface BrowserResultSet {
-      results: Array<TestResult>
+      results: Array<TestResult>;
     }
 
     export interface TestResult {
-      description: string,
-      suite: Array<string>,
-      success: boolean,
-      skipped: boolean,
-      pending: boolean,
-      time: number, // in milliseconds
+      description: string;
+      suite: Array<string>;
+      success: boolean;
+      skipped: boolean;
+      time: number; // in milliseconds
     }
   }
 }
