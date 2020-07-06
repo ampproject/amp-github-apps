05531fb58e4084d317d90017a5484752aa817cd8
rafer45 <ataque992@hotmail.com>
2020-07-06 13:36:35 -0400

    Implement cleaner query methods

--- test-case-reporting/src/db_interfacing.ts
+++ test-case-reporting/src/db_interfacing.ts
@@ -21,6 +21,7 @@ const msConversionConstant: number = Math.pow(10, 3 - TIMESTAMP_PRECISION);
 
 // Types in the DB namespace interface use snake_case instead of camelCase.
 /* eslint @typescript-eslint/camelcase: "off" */
+
 function getDateFromTimestamp(timestamp: number): Date {
   return new Date(timestamp * msConversionConstant);
 }
@@ -29,93 +30,154 @@ export class TestResultsRecords {
   constructor(private db: Database) {}
 
   /**
-   * Gets a DB.Build from the database by its name.
-   * If there is no build, it returns undefined.
-   * @param buildNumber The number of the Travis build, as a string
+   * Gets a DB.Build from the database by its build number.
+   * If there is no such build, it returns undefined.
+   * @param buildNumber The number of the Travis build, as a string.
    */
-  private async getDbBuild(buildNumber: string): Promise<DB.Build> {
+  private async getDbBuildFromBuildNumber(
+    buildNumber: string
+  ): Promise<DB.Build> {
     return this.db<DB.Build>('builds')
       .where('build_number', buildNumber)
       .first();
   }
 
-  private async getBuildWithEmptyJobs(buildNumber: string): Promise<Build> {
-    const dbBuild = await this.getDbBuild(buildNumber);
+  /**
+   * Gets a DB.Build from the database by its id.
+   * If there is no such build, it returns undefined.
+   * @param buildId The database id of the Travis build, as a number.
+   */
+  private async getDbBuildFromId(buildId: number): Promise<DB.Build> {
+    return this.db<DB.Build>('builds').where('id', buildId).first();
+  }
+
+  /**
+   * Makes a Build from a DB.Build.
+   * @param dbBuild A build as obtained from the database.
+   */
+  private getBuildFromDbBuild(dbBuild: DB.Build): Build {
     return {
       commitSha: dbBuild.commit_sha,
       buildNumber: dbBuild.build_number,
       startedAt: getDateFromTimestamp(dbBuild.started_at),
-      jobs: [],
-    };
-  }
-
-  private makeTestCaseFromRow(row): TestCase {
-    return {
-      name: row.test_case_name,
-      createdAt: getDateFromTimestamp(row.test_case_created_at),
-    };
-  }
-
-  private makeTestRunFromRow(row): TestRun {
-    return {
-      testCase: this.makeTestCaseFromRow(row),
-      status: row.test_run_status,
-      timestamp: getDateFromTimestamp(row.test_run_timestamp),
-      durationMs: row.duration_ms,
     };
   }
 
   /**
-   * Helper method.
-   * Takes a row of the join of every table.
-   * Makes a job containing only the test run described in that row.
-   * @param row A row of the join of every table, with some columns
-   * renamed for clarity.
+   * Gets a Build from the database by its build number.
+   * If there is no such build, it returns undefined.
+   * @param buildNumber The number of the Travis build, as a string
    */
-  private makeJobFromRow(row): Job {
+  private async getBuildFromBuildNumber(buildNumber: string): Promise<Build> {
+    const dbBuild = await this.getDbBuildFromBuildNumber(buildNumber);
+    return this.getBuildFromDbBuild(dbBuild);
+  }
+
+  /**
+   * Gets a Build from the database by its id.
+   * If there is no such build, it returns undefined.
+   * @param buildId The database id of the Travis build, as a number.
+   */
+  private async getBuildFromId(buildId: number): Promise<Build> {
+    const dbBuild = await this.getDbBuildFromId(buildId);
+    return this.getBuildFromDbBuild(dbBuild);
+  }
+
+  /**
+   * Gets a DB.Job from the database by its id.
+   * If there is no such job, it returns undefined.
+   * @param jobId The database id of the Travis job, as a number.
+   */
+  private async getDbJobFromId(jobId: number): Promise<DB.Job> {
+    return this.db<DB.Job>('jobs').where('id', jobId).first();
+  }
+
+  /**
+   * Gets a Job from the database by its id.
+   * If there is no such job, it returns undefined.
+   * @param jobId The database id of the Travis job, as a number.
+   */
+  private async getJobFromId(jobId: number): Promise<Job> {
+    const dbJob = await this.getDbJobFromId(jobId);
+    return this.getJobFromDbJob(dbJob);
+  }
+
+  /**
+   * Makes a Job from a DB.Job. Asynchronous because it needs to
+   * query the database for the build information.
+   * @param dbJob A job as obtained from the database.
+   */
+  private async getJobFromDbJob(dbJob: DB.Job): Promise<Job> {
     return {
-      jobNumber: row.job_number,
-      testSuiteType: row.test_suite_type,
-      startedAt: getDateFromTimestamp(row.job_started_at),
-      testRuns: [this.makeTestRunFromRow(row)],
+      build: await this.getBuildFromId(dbJob.build_id),
+      jobNumber: dbJob.job_number,
+      testSuiteType: dbJob.test_suite_type,
+      startedAt: getDateFromTimestamp(dbJob.started_at),
     };
   }
 
-  async getBuild(buildNumber: string): Promise<Build> {
-    const build = await this.getBuildWithEmptyJobs(buildNumber);
-    const joinOfEveryTable = await this.db('builds')
-      .select([
-        'test_runs.job_id',
-        'jobs.started_at as job_started_at',
-        'jobs.test_suite_type',
-        'jobs.job_number',
-        'jobs.job_number',
+  /**
+   * Makes a TestCase from a DB.TestCase.
+   * @param dbTestCase A test case as obtained from the database.
+   */
+  private getTestCaseFromDbTestCase(dbTestCase: DB.TestCase): TestCase {
+    return {
+      name: dbTestCase.name,
+      createdAt: getDateFromTimestamp(dbTestCase.created_at),
+    };
+  }
 
-        'test_cases.name as test_case_name',
-        'test_cases.created_at as test_case_created_at',
+  /**
+   * Gets a TestCase from the database by its id.
+   * If there is no such test case, it returns undefined.
+   * @param testCaseId The database id of the test case, as a number.
+   */
+  private async getTestCaseFromId(testCaseId: number): Promise<TestCase> {
+    const dbTestCase = await this.db<DB.TestCase>('test_cases')
+      .where('id', testCaseId)
+      .first();
+    return this.getTestCaseFromDbTestCase(dbTestCase);
+  }
 
-        'test_runs.status as test_run_status',
-        'test_runs.timestamp as test_run_timestamp',
-        'test_runs.duration_ms as test_run_duration_ms',
-      ])
-      .where('build_number', buildNumber)
-      .join('jobs', 'jobs.build_id', 'builds.id')
-      .join('test_runs', 'test_runs.job_id', 'jobs.id')
-      .join('test_cases', 'test_cases.id', 'test_runs.test_case_id');
+  /**
+   * Gets every DB.TestRun associated with a certain build, identified by its build number.
+   * If no build corresponds to the number, this returns an empty list.
+   * @param buildNumber The number of the Travis build whose test runs we want, as a string.
+   */
+  private async getDbTestRunsFromBuildNumber(
+    buildNumber: string
+  ): Promise<DB.TestRun[]> {
+    const buildId = (await this.getDbBuildFromBuildNumber(buildNumber)).id;
+    return this.db<DB.TestRun>('test_runs').where('build_id', buildId);
+  }
 
-    const jobs = joinOfEveryTable
-      .reduce((jobDict, row) => {
-        if (!jobDict[row.job_id]) {
-          jobDict[row.job_id] = this.makeJobFromRow(row);
-        } else {
-          jobDict[row.job_id].push(this.makeTestCaseFromRow(row));
-        }
-        return jobDict;
-      }, {})
-      .values();
+  /**
+   * Makes a TestRun from a DB.TestRun. Asynchronous because it needs to
+   * query the database for the job & test case information.
+   * @param dbTestRun A test run as obtained from the database.
+   */
+  private async getTestRunFromDbTestRun(
+    dbTestRun: DB.TestRun
+  ): Promise<TestRun> {
+    return {
+      job: await this.getJobFromId(dbTestRun.job_id),
+      testCase: await this.getTestCaseFromId(dbTestRun.test_case_id),
+      status: dbTestRun.status,
+      timestamp: getDateFromTimestamp(dbTestRun.timestamp),
+      durationMs: dbTestRun.duration_ms,
+    };
+  }
 
-    build.jobs = jobs;
-
-    return build;
+  /**
+   * Gets every TestRun associated with a certain build, identified by its build number.
+   * If no build corresponds to the number, this returns an empty list.
+   * @param buildNumber The number of the Travis build whose test runs we want, as a string.
+   */
+  public async getTestRunsFromBuildNumber(
+    buildNumber: string
+  ): Promise<TestRun[]> {
+    const dbTestRuns = await this.getDbTestRunsFromBuildNumber(buildNumber);
+    const testRunPromises = dbTestRuns.map(this.getTestRunFromDbTestRun);
+    return Promise.all(testRunPromises);
   }
 }
