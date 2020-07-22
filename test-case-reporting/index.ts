d7a86de78c5f1b8a219b082c8d7f3e6459639475
Rafer45 <ataque992@hotmail.com>
2020-07-22 17:04:43 -0400

    [test-case-reporting] Store reports that are POSTed to endpoint (#942)
    * Implement simple storing on POSTs
    
    * Add basic error handling
    
    * Use status codes, validate report
    
    * Remove unnecessary type import

--- test-case-reporting/index.ts
+++ test-case-reporting/index.ts
@@ -16,10 +16,17 @@
 
 require('dotenv').config();
 
-import {KarmaReporter} from 'test-case-reporting';
+import {TestResultRecord} from './src/test_result_record';
+import {Travis} from 'test-case-reporting';
+import {dbConnect} from './src/db';
 import express from 'express';
+import statusCodes from 'http-status-codes';
+
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+const db = dbConnect();
+const record = new TestResultRecord(db);
 
 app.get('/', (req, res) => {
   res.send('Hello, world, 2.0!');
@@ -35,9 +42,30 @@ app.get('/test-results/history/:testCaseId', (req, res) => {
   res.send(`Test history for test with name/ID ${testCaseId}`);
 });
 
-app.post('/report', (req, res) => {
-  const report: KarmaReporter.TestResultReport = req.body;
-  res.send(`Travis report! The request body is below.\n\n${report}`);
+app.post('/report', async (req, res) => {
+  try {
+    const report: Travis.Report = req.body;
+    const topLevelKeys: Array<keyof Travis.Report> = ['job', 'build', 'result'];
+
+    for (const key of topLevelKeys) {
+      if (!(report[key] as unknown)) {
+        throw new TypeError(`Report payload must include ${key} property`);
+      }
+    }
+
+    await record.storeTravisReport(report);
+    res.sendStatus(statusCodes.CREATED);
+  } catch (error) {
+    console.warn(error);
+    if (error instanceof TypeError) {
+      // Missing top-level keys from the report.
+      res.status(statusCodes.BAD_REQUEST);
+    } else {
+      res.status(statusCodes.INTERNAL_SERVER_ERROR);
+    }
+
+    res.json({error: error.toString()});
+  }
 });
 
 app.listen(PORT, () => {
