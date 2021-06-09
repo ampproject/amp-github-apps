c95caef1acd698ac246d1c13a2efccf04a601c0b
Riley Jones <rileyjones@google.com>
2021-06-09 13:37:27 -0700

    fix cors issue

--- release-calendar/src/server/app.ts
+++ release-calendar/src/server/app.ts
@@ -43,10 +43,12 @@ async function main(): Promise<void> {
   const app = express();
   const port = process.env.PORT;
 
-  app.use(function (req, res, next) {
+  app.use(function (_req, res, next) {
     res.header(
       'Access-Control-Allow-Origin',
-      `${process.env.CLIENT_URL}:${process.env.CLIENT_PORT}`,
+      process.env.NODE_ENV === 'production'
+        ? `${process.env.CLIENT_URL}`
+        : `${process.env.CLIENT_URL}:${process.env.CLIENT_PORT}`,
     );
     res.header(
       'Access-Control-Allow-Headers',
